af0782ba893c5d464c8a147e1bbb761a67e82b6c
Jamie Diprose <jamie.diprose@gmail.com>
2020-01-29 10:10:53 +1300

    Add docstrings to functions

--- academic_observatory/utils/argparse_utils.py
+++ academic_observatory/utils/argparse_utils.py
@@ -4,6 +4,11 @@ import os
 
 
 def validate_path(string: str):
+    """ Validate whether the given string is a valid path.
+    :param string: a string that might be a file path.
+    :return: the string if it validated otherwise raise an argparse.ArgumentTypeError.
+    """
+
     if not os.path.exists(string):
         msg = f"Path does not exist: {string}"
         logging.error(msg)
