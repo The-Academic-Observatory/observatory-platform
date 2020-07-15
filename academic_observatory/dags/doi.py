d5795c1f3317624d5358297281ffe1e5848e5c80
Richard Hosking <richard.hosking@curtin.edu.au>
2020-07-15 14:46:20 +1200

    mapped out addition airflow tasks

--- academic_observatory/dags/doi.py
+++ academic_observatory/dags/doi.py
@@ -17,16 +17,26 @@
 
 from datetime import datetime
 
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-from academic_observatory.workflows.dois import DoiWorkflow
+from academic_observatory.workflows.doi import DoiWorkflow
 
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2020, 1, 1)
 }
 
-with DAG(dag_id = DoiWorkflow.BAG_ID, schedule_interval="@once", default_args=default_args) as dag:
+with DAG(dag_id = DoiWorkflow.DAG_ID, schedule_interval="@once", default_args=default_args) as dag:
+
+    # Extend GRID with iso3166 and home repos
+    task_extend_grid_with_iso3166_and_home_repos = PythonOperator(
+        task_id=DoiWorkflow.TASK_ID_EXTEND_GRID_WITH_ISO3166_AND_HOME_REPOS,
+        provide_context=True,
+        python_callable=DoiWorkflow.extend_grid_with_iso3166_and_home_repos,
+        dag=dag,
+        depends_on_past=True
+    )
 
     # Aggregrate Crossref Events
     task_aggregate_crossref_events = PythonOperator(
@@ -51,6 +61,42 @@ with DAG(dag_id = DoiWorkflow.BAG_ID, schedule_interval="@once", default_args=de
         task_id=DoiWorkflow.TASK_ID_COMPUTE_OA_COLOURS,
         provide_context=True,
         python_callable=DoiWorkflow.compute_oa_colours,
+        dag=dag,
+        depends_on_past=True
+    )
+
+    # Aggregrate Open Citations
+    task_aggregate_open_citations = PythonOperator(
+        task_id=DoiWorkflow.TASK_ID_AGGREGATE_OPEN_CITATIONS,
+        provide_context=True,
+        python_callable=DoiWorkflow.aggregate_open_citations,
+        dag=dag,
+        depends_on_past=True
+    )
+
+    # Aggregrate WoS
+    task_aggregate_wos = PythonOperator(
+        task_id=DoiWorkflow.TASK_ID_AGGREGATE_WOS,
+        provide_context=True,
+        python_callable=DoiWorkflow.aggregate_wos,
+        dag=dag,
+        depends_on_past=True
+    )
+
+    # Aggregrate Scopus
+    task_aggregate_scopus = PythonOperator(
+        task_id=DoiWorkflow.TASK_ID_AGGREGATE_SCOPUS,
+        provide_context=True,
+        python_callable=DoiWorkflow.aggregate_scopus,
+        dag=dag,
+        depends_on_past=True
+    )
+
+    # Build DOIs table
+    task_build_dois_table = PythonOperator(
+        task_id=DoiWorkflow.TASK_ID_BUILD_DOIS_TBLE,
+        provide_context=True,
+        python_callable=DoiWorkflow.build_dois_table,
         dag=dag,
         depends_on_past=True
     )
