00d9c5244cff6d75e5b48565b3f2e034a65cb52a
Tuan Chien <tuan@couch.science>
2020-09-23 15:15:28 +1200

    ScopusTelescope: change pipeline to ETL subdags for each institution (like wos)

--- observatory_platform/dags/scopus.py
+++ observatory_platform/dags/scopus.py
@@ -15,6 +15,8 @@
 # Author: Tuan Chien
 
 
+import logging
+
 from datetime import datetime
 
 from airflow import DAG
@@ -30,10 +32,10 @@ from airflow.models import Connection
 
 
 def list_connections(source):
-    """Get a list of data source connections with name starting with <source>_, e.g., wos_curtin.
+    """Get a list of data source connections with name starting with <source>_, e.g., scopus_curtin.
 
-    :param source: Data source (conforming to name convention) as a string, e.g., 'wos'.
-    :return: A list of connection id strings with the prefix <source>_, e.g., ['wos_curtin', 'wos_auckland'].
+    :param source: Data source (conforming to name convention) as a string, e.g., 'scopus'.
+    :return: A list of connection id strings with the prefix <source>_, e.g., ['scopus_curtin', 'scopus_auckland'].
     """
     with create_session() as session:
         query = session.query(Connection)
@@ -46,110 +48,114 @@ default_args = {'owner': 'airflow',
                 }
 
 
-def download_subdag_factory(parent_dag_id, subdag_id, args):
-    """ Factory for making the download subdag.
+def subdag_factory(parent_dag_id, connection, args):
+    """ Factory for making the ETL subdags.
 
     :param parent_dag_id: parent dag's id.
-    :param subdag_id: id of this subdag.
+    :param connection: Airflow Connection object.
     :param args: default arguments to use for this subdag.
     :return: DAG object.
     """
 
+    institution = str(connection)[4:]
+    logging.info(f'Spawning ETL subdag for: {institution}')
+
     subdag = DAG(
-        dag_id=f'{parent_dag_id}.{subdag_id}',
+        dag_id=f'{parent_dag_id}.{institution}',
         default_args=args,
         catchup=False,
         schedule_interval=ScopusTelescope.SCHEDULE_INTERVAL
     )
 
-    conns = list_connections('wos')
-
     with subdag:
-        # Load a new task on the subdag for every institution.
-        for conn in conns:
-            institution = str(conn)[4:]
-            PythonOperator(
-                task_id=institution,
-                python_callable=ScopusTelescope.download,
-                op_kwargs={'conn': conn, 'dag_start': '{{dag_run.start_date}}'},
-                provide_context=True,
-                queue=ScopusTelescope.QUEUE,
-                retries=ScopusTelescope.RETRIES,
-            )
+        # Check that dependencies exist before starting
+        check_dependencies = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_CHECK_DEPENDENCIES,
+            python_callable=ScopusTelescope.check_dependencies,
+            op_kwargs={'dag_start': '{{dag_run.start_date}}', 'conn': connection, 'institution': institution},
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE
+        )
+
+        download = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_DOWNLOAD,
+            python_callable=ScopusTelescope.download,
+            op_kwargs={'conn': connection},
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE,
+            retries=ScopusTelescope.RETRIES
+        )
+
+        # Upload gzipped response (json)
+        upload_downloaded = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_UPLOAD_DOWNLOADED,
+            python_callable=ScopusTelescope.upload_downloaded,
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE,
+            retries=ScopusTelescope.RETRIES
+        )
+
+        # Transform into database schema format (jsonline output)
+        transform_db_format = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_TRANSFORM_DB_FORMAT,
+            python_callable=ScopusTelescope.transform_db_format,
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE
+        )
+
+        # # Upload the gzipped transformed jsonline entries to Google Cloud Storage
+        upload_transformed = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_UPLOAD_TRANSFORMED,
+            python_callable=ScopusTelescope.upload_transformed,
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE,
+            retries=ScopusTelescope.RETRIES
+        )
+
+        # Load the transformed WoS snapshot to BigQuery
+        # Depends on past so that BigQuery load jobs are not all created at once
+        bq_load = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_BQ_LOAD,
+            python_callable=ScopusTelescope.bq_load,
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE
+        )
+
+        cleanup = PythonOperator(
+            task_id=ScopusTelescope.TASK_ID_CLEANUP,
+            python_callable=ScopusTelescope.cleanup,
+            provide_context=True,
+            queue=ScopusTelescope.QUEUE
+        )
+
+        check_dependencies >> download >> upload_downloaded >> transform_db_format >> upload_transformed \
+        >> bq_load >> cleanup
 
     return subdag
 
 
 with DAG(dag_id=ScopusTelescope.DAG_ID, schedule_interval=ScopusTelescope.SCHEDULE_INTERVAL, catchup=False,
          default_args=default_args) as dag:
-    # Check that dependencies exist before starting
-
-    check_dependencies = PythonOperator(
-        task_id=ScopusTelescope.TASK_ID_CHECK_DEPENDENCIES,
-        python_callable=ScopusTelescope.check_dependencies,
-        provide_context=True,
-        queue=ScopusTelescope.QUEUE
-    )
-
-    # Only process if the SCOPUS API server is up.
+    # Only process if the Web of Science API server is up.
     check_api_server = PythonOperator(
         task_id=ScopusTelescope.TASK_CHECK_API_SERVER,
         python_callable=ScopusTelescope.check_api_server,
-        provide_context=True,
+        provide_context=False,
         queue=ScopusTelescope.QUEUE,
         retries=ScopusTelescope.RETRIES,
     )
 
-    # Download the SCOPUS snapshot for all configured institutions
-    download = SubDagOperator(
-        task_id=ScopusTelescope.TASK_ID_DOWNLOAD,
-        subdag=download_subdag_factory(ScopusTelescope.DAG_ID, ScopusTelescope.SUBDAG_ID_DOWNLOAD, default_args),
-        default_args=default_args,
-        dag=dag
-    )
+    # Spawn a subdag to handle ETL for each institution
+    subdags = list()
+    conns = list_connections('wos')
+    for conn in conns:
+        subdag = SubDagOperator(
+            task_id=str(conn)[4:],
+            subdag=subdag_factory(ScopusTelescope.DAG_ID, conn, default_args),
+            default_args=default_args,
+            dag=dag
+        )
 
-    # Upload gzipped response (JSON)
-    upload_downloaded = PythonOperator(
-        task_id=ScopusTelescope.TASK_ID_UPLOAD_DOWNLOADED,
-        python_callable=ScopusTelescope.upload_downloaded,
-        provide_context=True,
-        queue=ScopusTelescope.QUEUE,
-        retries=ScopusTelescope.RETRIES
-    )
-
-    # Transform into database schema format
-    transform_db_format = PythonOperator(
-        task_id=ScopusTelescope.TASK_ID_TRANSFORM_DB_FORMAT,
-        python_callable=ScopusTelescope.transform_db_format,
-        provide_context=True,
-        queue=ScopusTelescope.QUEUE
-    )
-
-    # # Upload the transformed jsonline entries to Google Cloud Storage
-    upload_transformed = PythonOperator(
-        task_id=ScopusTelescope.TASK_ID_UPLOAD_TRANSFORMED,
-        python_callable=ScopusTelescope.upload_transformed,
-        provide_context=True,
-        queue=ScopusTelescope.QUEUE,
-        retries=ScopusTelescope.RETRIES
-    )
-
-    # Load the transformed SCOPUS snapshot to BigQuery
-    # Depends on past so that BigQuery load jobs are not all created at once
-    bq_load = PythonOperator(
-        task_id=ScopusTelescope.TASK_ID_BQ_LOAD,
-        python_callable=ScopusTelescope.bq_load,
-        provide_context=True,
-        queue=ScopusTelescope.QUEUE
-    )
-
-    cleanup = PythonOperator(
-        task_id=ScopusTelescope.TASK_ID_CLEANUP,
-        python_callable=ScopusTelescope.cleanup,
-        provide_context=True,
-        queue=ScopusTelescope.QUEUE
-    )
-
-    # Task dependencies
-    check_dependencies >> check_api_server >> download >> upload_downloaded \
-        >> transform_db_format >> upload_transformed >> bq_load >> cleanup
+        # Task dependencies
+        check_api_server >> subdag
+        subdags.append(subdag)
