064d7f40def3e8192147477a71bedc2ec13e3d6a
Tuan Chien <tuan@couch.science>
2020-09-24 12:08:13 +1200

    ScopusTelescope: add test for ScopusRelease

--- tests/observatory_platform/telescopes/test_scopus.py
+++ tests/observatory_platform/telescopes/test_scopus.py
@@ -18,35 +18,80 @@
 import glob
 import logging
 import os
+import pendulum
 import unittest
-from typing import List, Dict
-
 import vcr
+
+from typing import List, Dict
 from click.testing import CliRunner
+from unittest.mock import patch
+
+from observatory_platform.telescopes.scopus import (
+    ScopusRelease
+)
 
 
-class TestScopus(unittest.TestCase):
-    """ Tests for the functions used by the SCOPUS telescope """
+class TestScopusRelease(unittest.TestCase):
+    """ Test the ScopusRelease class. """
 
-    def __init__(self, *args, **kwargs):
-        """ Constructor which sets up variables used by tests.
-
-        :param args: arguments.
-        :param kwargs: keyword arguments.
-        """
-
-        super(TestScopus, self).__init__(*args, **kwargs)
-
-        # Paths
-        self.work_dir = '.'
-
-        # Release <date>
+    # def __init__(self, *args, **kwargs):
+    #     """ Constructor which sets up variables used by tests.
+    #
+    #     :param args: arguments.
+    #     :param kwargs: keyword arguments.
+    #     """
+    #
+    #     super(TestScopusRelease, self).__init__(*args, **kwargs)
 
 
-        # Turn logging to warning because vcr prints too much at info level
-        logging.basicConfig()
-        logging.getLogger().setLevel(logging.WARNING)
+    @patch('observatory_platform.utils.config_utils.airflow.models.Variable.get', return_value='teletest')
+    def test_init(self, tele):
+        """ Test initialisation. """
+
+        obj = ScopusRelease(inst_id='inst_id', scopus_inst_id=['scopus_inst_id'],
+                            release_date=pendulum.date(1984, 1, 1),
+                            dag_start=pendulum.date(2000, 1, 1), project_id='project_id',
+                            download_bucket_name='download_bucket', transform_bucket_name='transform_bucket',
+                            data_location='data_location', schema_ver='schema_ver')
 
 
-    def test_stub(self):
-        """ Placeholder for test. """
+        self.assertEqual(obj.inst_id, 'inst_id')
+        self.assertEqual(obj.scopus_inst_id[0], 'scopus_inst_id')
+        self.assertEqual(obj.release_date, pendulum.date(1984, 1, 1))
+        self.assertEqual(obj.dag_start, pendulum.date(2000, 1, 1))
+        self.assertEqual(obj.project_id, 'project_id')
+        self.assertEqual(obj.download_path, 'teletest/telescopes/download/scopus')
+        self.assertEqual(obj.transform_path, 'teletest/telescopes/transform/scopus')
+        self.assertEqual(obj.telescope_path, 'telescopes/scopus/1984-01-01')
+        self.assertEqual(obj.download_bucket_name, 'download_bucket')
+        self.assertEqual(obj.transform_bucket_name, 'transform_bucket')
+        self.assertEqual(obj.data_location, 'data_location')
+        self.assertEqual(obj.schema_ver, 'schema_ver')
+        self.assertEqual(tele.call_count, 1)
+
+# class TestScopus(unittest.TestCase):
+#     """ Tests for the functions used by the SCOPUS telescope """
+#
+#     def __init__(self, *args, **kwargs):
+#         """ Constructor which sets up variables used by tests.
+#
+#         :param args: arguments.
+#         :param kwargs: keyword arguments.
+#         """
+#
+#         super(TestScopus, self).__init__(*args, **kwargs)
+#
+#         # Paths
+#         self.work_dir = '.'
+#
+#         # Release <date>
+#
+#
+#         # Turn logging to warning because vcr prints too much at info level
+#         logging.basicConfig()
+#         logging.getLogger().setLevel(logging.WARNING)
+#
+#
+#     def test_build_query(self):
+#         """ Test query building code. """
+#
