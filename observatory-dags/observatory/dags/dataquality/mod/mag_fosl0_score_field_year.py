4f98bd478026b5bc8ff10f277c836ba2e476dd9f
Tuan Chien <tuan@couch.science>
2020-12-09 12:41:39 +1300

    DQ FosL0ScoreFieldYear: use a single bq query to get data

--- observatory-dags/observatory/dags/dataquality/mod/mag_fosl0_score_field_year.py
+++ observatory-dags/observatory/dags/dataquality/mod/mag_fosl0_score_field_year.py
@@ -43,8 +43,8 @@ class FosL0ScoreFieldYearModule(MagAnalyserModule):
         Generates MagFosL0ScoreFieldYear elastic search documents.
     """
 
-    YEAR_START = 1800
-    BUCKET_START = 0.0
+    YEAR_START = 2000
+    BUCKET_START = 0.01
     BUCKET_END = 1.0
     BUCKET_STEP = 0.01
 
@@ -75,29 +75,14 @@ class FosL0ScoreFieldYearModule(MagAnalyserModule):
 
         logging.info(f'Running {self.name()}')
         releases = self._cache[MagCacheKey.RELEASES]
-        year_end = datetime.datetime.now(datetime.timezone.utc).year
 
-        with ThreadPoolExecutor(max_workers=MagParams.BQ_SESSION_LIMIT) as executor:
-            for release in releases:
-                ts = release.strftime('%Y%m%d')
-                fos_ids = self._cache[f'{MagCacheKey.FOSL0}{ts}']
+        for release in releases:
+            if search_count_by_release(MagFosL0ScoreFieldYear, release.isoformat()) > 0:
+                continue
 
-                if search_count_by_release(MagFosL0ScoreFieldYear, release.isoformat()) > 0:
-                    continue
-
-                for fos in fos_ids:
-                    docs = list()
-                    futures = list()
-                    logging.info(f'Computing release: {ts}, fos: {fos[1]}')
-
-                    for year in range(FosL0ScoreFieldYearModule.YEAR_START, year_end + 1):
-                        futures.append(executor.submit(self._construct_es_docs, release, ts, fos, year))
-
-                    for future in as_completed(futures):
-                        docs.extend(future.result())
-
-                    if len(docs) > 0:
-                        bulk_index(docs)
+            docs = self._construct_es_docs(release)
+            if len(docs) > 0:
+                bulk_index(docs)
 
     def erase(self, index: bool = False, **kwargs):
         """
@@ -110,50 +95,83 @@ class FosL0ScoreFieldYearModule(MagAnalyserModule):
         if index:
             delete_index(MagFosL0ScoreFieldYear)
 
-    def _construct_es_docs(self, release: datetime.date, ts: str, fos: Tuple[int, str], year: int) -> List[Document]:
-        """ Construct MagFosL0ScoreFieldYear docs for each release.
-
-        @param release: Release date.
-        @param ts: Table suffix (timestamp).
-        @param fos: FieldOfStudyId, Normalized Name.
-        @param year: Publication year we're interested in.
-        @return List of constructed elastic search documents.
+    def _new_histogram(self):
+        """ @return: A histogram of 0s
         """
 
-        docs = list()
-        year = str(year)
-        counts = self._get_bq_counts(ts, fos[0], year)
         num_buckets = int((FosL0ScoreFieldYearModule.BUCKET_END - FosL0ScoreFieldYearModule.BUCKET_START) \
                           / FosL0ScoreFieldYearModule.BUCKET_STEP)
         histogram = [0] * num_buckets
 
+        return histogram
+
+    def _get_histograms_dict(self, counts: pd.DataFrame) -> dict:
+        """ Initialise a histogram dictionary with the values from bigquery.
+            histogram[fos_id][year][bucket_index]
+
+            @param counts: Query response of histograms.
+            @return: Histogram dictionary.
+        """
+
+        histograms = {}
+
         for i in range(len(counts)):
-            bucket = counts[FosL0ScoreFieldYearModule.BQ_BUCKET][i] - 1
+            fos_id = counts[MagTableKey.COL_FOS_ID][i]
+            year = counts[MagTableKey.COL_YEAR][i]
+            bucket = counts[FosL0ScoreFieldYearModule.BQ_BUCKET][i]
             count = counts[FosL0ScoreFieldYearModule.BQ_COUNT][i]
-            histogram[bucket] = count
 
-        for i in range(len(histogram)):
-            doc = MagFosL0ScoreFieldYear(release=release, field_id=fos[0], field_name=fos[1], year=year,
-                                         count=histogram[i],
-                                         score_start=i * FosL0ScoreFieldYearModule.BUCKET_STEP,
-                                         score_end=(i + 1) * FosL0ScoreFieldYearModule.BUCKET_STEP)
-            docs.append(doc)
+            if fos_id not in histograms:
+                histograms[fos_id] = {}
 
-        self._cache[f'{MagCacheKey.FOSL0_FIELD_YEAR_SCORES}{ts}-{fos[0]}-{year}'] = histogram
+            if year not in histograms[fos_id]:
+                histograms[fos_id][year] = self._new_histogram()
+
+            histograms[fos_id][year][bucket] = count
+
+        return histograms
+
+
+    def _construct_es_docs(self, release: datetime.date) -> List[Document]:
+        """ Construct MagFosL0ScoreFieldYear docs for each release.
+
+        @param release: Release date.
+        @return: List of constructed elastic search documents.
+        """
+
+        ts = release.strftime('%Y%m%d')
+        fosl0 = self._cache[f'{MagCacheKey.FOSL0}{ts}']
+        fos_ids = (fos[0] for fos in fosl0)
+        fos_lookup = { fos[0]: fos[1] for fos in fosl0 }
+
+        docs = list()
+        counts = self._get_bq_counts(ts, fos_ids)
+        histograms = self._get_histograms_dict(counts)
+
+        for fos_id in histograms:
+            for year in histograms[fos_id]:
+                for bucket in histograms[fos_id][year]:
+                    doc = MagFosL0ScoreFieldYear(release=release, year=year, count=histograms[fos_id][year][bucket],
+                                                 field_id=fos_id, field_name=fos_lookup[fos_id],
+                                                 score_start=bucket * FosL0ScoreFieldYearModule.BUCKET_STEP,
+                                                 score_end=(bucket + 1) * FosL0ScoreFieldYearModule.BUCKET_STEP)
+
+                    docs.append(doc)
+                self._cache[f'{MagCacheKey.FOSL0_FIELD_YEAR_SCORES}{ts}-{fos_id}-{year}'] = histograms[fos_id][year][bucket]
 
         return docs
 
-    def _get_bq_counts(self, ts: str, fos_id: int, year: int) -> pd.DataFrame:
+    def _get_bq_counts(self, ts: str, fos_ids: Tuple[int]) -> pd.DataFrame:
         """ Get counts from BigQuery
 
         @param ts: Table suffix (timestamp).
-        @param fos_id: FieldOfStudyId.
-        @param year: Publication year we're interested in.
+        @param fos_ids: List of FieldOfStudyId we're interested in.
         @return DataFrame of counts.
         """
 
-        sql = self._tpl_histogram.render(project_id=self._project_id, dataset_id=self._dataset_id,
-                                         ts=ts, fos_id=fos_id, year=year, count=FosL0ScoreFieldYearModule.BQ_COUNT,
+        sql = self._tpl_histogram.render(project_id=self._project_id, dataset_id=self._dataset_id, ts=ts,
+                                         fos_ids=fos_ids, year=FosL0ScoreFieldYearModule.YEAR_START,
+                                         count=FosL0ScoreFieldYearModule.BQ_COUNT,
                                          bstart=FosL0ScoreFieldYearModule.BUCKET_START + FosL0ScoreFieldYearModule.BUCKET_STEP,
                                          bend=FosL0ScoreFieldYearModule.BUCKET_END,
                                          bstep=FosL0ScoreFieldYearModule.BUCKET_STEP)
