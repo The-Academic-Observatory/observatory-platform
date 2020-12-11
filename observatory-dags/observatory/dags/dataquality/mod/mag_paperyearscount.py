297b5ce44168fb89f2f585e2c1a12420c250770b
Tuan Chien <tuan@couch.science>
2020-12-11 16:00:40 +1300

    mag_paperyearscount: adding logging and comments

--- observatory-dags/observatory/dags/dataquality/mod/mag_paperyearscount.py
+++ observatory-dags/observatory/dags/dataquality/mod/mag_paperyearscount.py
@@ -25,6 +25,7 @@ from typing import Tuple, List
 from observatory.dags.dataquality.analyser import MagAnalyserModule
 from jinja2 import Environment, PackageLoader
 
+from observatory.dags.dataquality.autofetchcache import AutoFetchCache
 from observatory.dags.dataquality.utils import proportion_delta
 from observatory.dags.dataquality.config import JinjaParams, MagCacheKey, MagTableKey
 from observatory.dags.dataquality.es_mag import MagPapersYearCount
@@ -40,20 +41,25 @@ from observatory.dags.dataquality.es_utils import (
 class PaperYearsCountModule(MagAnalyserModule):
     """ MagAnalyser module to compute paper counts by year from MAG. """
 
-    def __init__(self, project_id: str, dataset_id: str, cache):
+    BQ_COUNT = 'count'  # SQL column for the counts
+    YEAR_START = 2000  # Year to start fetching information from.
+
+    def __init__(self, project_id: str, dataset_id: str, cache: AutoFetchCache):
         """ Initialise the module.
         @param project_id: Project ID in BigQuery.
         @param dataset_id: Dataset ID in BigQuery.
         @param cache: Analyser cache to use.
         """
 
-        logging.info(f'Initialising {self.name()}')
+        logging.info(f'{self.name()}: initialising.')
         self._project_id = project_id
         self._dataset_id = dataset_id
         self._cache = cache
+
         self._tpl_env = Environment(
             loader=PackageLoader(JinjaParams.PKG_NAME, JinjaParams.TEMPLATE_PATHS))
         self._tpl_group_count = self._tpl_env.get_template('group_count.sql.jinja2')
+
         init_doc(MagPapersYearCount)
 
     def run(self, **kwargs):
@@ -61,27 +67,29 @@ class PaperYearsCountModule(MagAnalyserModule):
         @param kwargs: Unused.
         """
 
-        logging.info(f'Running {self.name()}')
+        logging.info(f'{self.name()}: executing.')
         releases = self._cache[MagCacheKey.RELEASES]
         num_releases = len(releases)
 
         docs = list()
         for i in range(num_releases):
+            # If records exist in elastic search, skip.  This is not robust to partial records (past interrupted loads).
             if search_count_by_release(MagPapersYearCount, releases[i]) > 0:
                 continue
             year, counts = self._get_paper_year_count(releases[i])
 
-            # Proportional difference between years
+            # Proportional difference between years.
             prev = [0] + counts[:-1]
             delta = proportion_delta(counts, prev)
 
+            # Create the ES documents.
             for j in range(len(year)):
-                paper_count = MagPapersYearCount(release=releases[i].isoformat(), year=str(int(year[j])), count=counts[j],
-                                                 delta_pcount=delta[j], delta_count=counts[j]-prev[j])
+                paper_count = MagPapersYearCount(release=releases[i].isoformat(), year=str(int(year[j])),
+                                                 count=counts[j],
+                                                 delta_pcount=delta[j], delta_count=counts[j] - prev[j])
                 docs.append(paper_count)
 
-        logging.info(f'Constructed {len(docs)} MagPapersYearCount documents.')
-
+        logging.info(f'{self.name()}: indexing {len(docs)} docs of type MagPapersYearCount.')
         if len(docs) > 0:
             bulk_index(docs)
 
@@ -105,9 +113,13 @@ class PaperYearsCountModule(MagAnalyserModule):
 
         ts = release.strftime('%Y%m%d')
         table_id = f'{MagTableKey.TID_PAPERS}{ts}'
+
         sql = self._tpl_group_count.render(
             project_id=self._project_id, dataset_id=self._dataset_id, table_id=table_id,
-            column='Year', where='Year IS NOT NULL'
+            column=MagTableKey.COL_YEAR, count=PaperYearsCountModule.BQ_COUNT,
+            where=f'Year IS NOT NULL AND Year >= {PaperYearsCountModule.YEAR_START}'
         )
+
         df = pd.read_gbq(sql, project_id=self._project_id, progress_bar_type=None)
-        return df['Year'].to_list(), df['count'].to_list()
+
+        return df[MagTableKey.COL_YEAR].to_list(), df[PaperYearsCountModule.BQ_COUNT].to_list()
