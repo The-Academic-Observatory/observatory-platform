e840caa85d04c9fe41ced790805591c8d586db5e
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-01-08 17:38:14 +0800

    Initial commit for working cloud run/ cloud endpoints setup.

--- observatory-platform/observatory/platform/elasticsearch/server.py
+++ observatory-platform/observatory/platform/elasticsearch/server.py
@@ -9,19 +9,19 @@ import connexion
 # create the application instance
 app = connexion.App(__name__, specification_dir="./")
 
-# Cead the swagger.yml file to configure the endpoints
-app.add_api("swagger.yml")
+# Read the openapi.yml file to configure the endpoints
+app.add_api("openapi2.yml")
 
 
 # Create a URL route in our application for "/"
-# @app.route("/")
-# def home():
-#     """
-#     This function just responds to the browser URL
-#     localhost:5000/
-#     :return:        the rendered template "home.html"
-#     """
-#     return render_template("home.html")
+@app.route("/")
+def home():
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "home.html"
+    """
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
