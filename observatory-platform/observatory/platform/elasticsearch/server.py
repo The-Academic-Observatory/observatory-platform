c65e1127113496d2f9837658334772bd4096f2f7
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-01-11 18:03:49 +0800

    Add waypoint configuration. Replaced 'connexion' with  'connexion[swagger-ui]' in the requirements, so the swagger ui is visible when deployed on cloud run. Create query parameters using jinja2 for loop from list of parameters.

--- observatory-platform/observatory/platform/elasticsearch/server.py
+++ observatory-platform/observatory/platform/elasticsearch/server.py
@@ -2,15 +2,22 @@
 Main module of the server file
 """
 
-from flask import render_template
 import connexion
-
+from flask import render_template
 
 # create the application instance
 app = connexion.App(__name__, specification_dir="./")
 
-# Read the openapi.yml file to configure the endpoints
-app.add_api("openapi2.yml")
+query_filter_parameters = ['id', 'name', 'published_year', 'coordinates', 'country', 'country_code', 'region',
+                           'subregion', 'access_type', 'label', 'status', 'collaborator_coordinates',
+                           'collaborator_country', 'collaborator_country_code', 'collaborator_id', 'collaborator_name',
+                           'collaborator_region', 'collaborator_subregion', 'field', 'source', 'funder_country_code',
+                           'funder_name', 'funder_sub_type', 'funder_type', 'journal', 'output_type', 'publisher']
+
+# app.add_api(rendered_openapi)
+app.add_api('openapi2.yml', arguments={
+    'query_parameters': query_filter_parameters
+})
 
 
 # Create a URL route in our application for "/"
