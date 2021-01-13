95f48828dcce0c0678b73d885f14025b7648ed88
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-01-13 18:41:48 +0800

    Increased cloud run memory 128 -> 256 MB
    Add requirement for api key, either in query or in header. The existing keys are stored locally in a file.
    
    Changed response, added 'returned_results', 'total_results', 'schema' and 'results' to response. Also updated the response definition in the openapi file.
    
    Added a limit parameter, which limits the number of results returned.
    
    Defined the query filter parameters in a list that is stored in the app context, so it can be used in the 'query' module as well.

--- observatory-platform/observatory/platform/elasticsearch/app/server.py
+++ observatory-platform/observatory/platform/elasticsearch/app/server.py
@@ -4,17 +4,35 @@ Main module of the server file
 
 import connexion
 from flask import render_template
+from flask import current_app
+import os
+
+
+def basic_auth(api_key, required_scopes=None):
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(script_directory, 'api', 'elasticsearch_auth.txt')) as auth_file:
+        for line in auth_file:
+            username, token = line.strip().split(' ')
+            if api_key == token:
+                return {'sub': username}
+    return None
+
 
 # create the application instance
 app = connexion.App(__name__, specification_dir="./")
+app.app.config['JSON_SORT_KEYS'] = False
 
 query_filter_parameters = ['id', 'name', 'published_year', 'coordinates', 'country', 'country_code', 'region',
-                           'subregion', 'access_type', 'label', 'status', 'collaborator_coordinates',
-                           'collaborator_country', 'collaborator_country_code', 'collaborator_id', 'collaborator_name',
-                           'collaborator_region', 'collaborator_subregion', 'field', 'source', 'funder_country_code',
-                           'funder_name', 'funder_sub_type', 'funder_type', 'journal', 'output_type', 'publisher']
+                         'subregion',
+                'access_type', 'label', 'status', 'collaborator_coordinates', 'collaborator_country',
+                'collaborator_country_code', 'collaborator_id', 'collaborator_name', 'collaborator_region',
+                'collaborator_subregion', 'field', 'source', 'funder_country_code', 'funder_name', 'funder_sub_type',
+                'funder_type', 'journal', 'output_type', 'publisher']
 
 app.add_api('openapi2.yml', arguments={'query_parameters': query_filter_parameters})
+
+with app.app.app_context():
+    current_app.query_filter_parameters = query_filter_parameters
 
 
 # Create a URL route in our application for "/"
