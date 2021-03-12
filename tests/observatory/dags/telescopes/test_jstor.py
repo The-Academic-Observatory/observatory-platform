2c910e3a9f23520732cd8679f48f4ffaed3b2ede
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-03-12 16:48:17 +0800

    Add unit tests

--- tests/observatory/dags/telescopes/test_jstor.py
+++ tests/observatory/dags/telescopes/test_jstor.py
@@ -15,21 +15,34 @@
 # Author: Aniek Roelofs
 
 import os
+from typing import Any, Dict
+from unittest.mock import Mock, patch
 
-import httpretty
 import pendulum
-import vcr
+from airflow.models.connection import Connection
+from observatory.dags.telescopes.jstor import (JstorRelease, JstorTelescope)
+from observatory.platform.utils.airflow_utils import AirflowConns
+from observatory.platform.utils.template_utils import blob_name, table_ids_from_path
+from observatory.platform.utils.test_utils import ObservatoryEnvironment, ObservatoryTestCase
 from pendulum import Pendulum
 
-from observatory.dags.telescopes.jstor import (JstorRelease, JstorTelescope)
-from observatory.platform.utils.file_utils import _hash_file
-from observatory.platform.utils.gc_utils import bigquery_partitioned_table_id
-from observatory.platform.utils.template_utils import telescope_path, SubFolder, blob_name
-from observatory.platform.utils.test_utils import ObservatoryEnvironment, ObservatoryTestCase
-from tests.observatory.test_utils import test_fixtures_path
-from observatory.platform.utils.airflow_utils import AirflowConns
-from airflow.models.connection import Connection
-from observatory.platform.telescopes.snapshot_telescope import SnapshotRelease
+
+class MockSftpService(Mock):
+    def __init__(self, files: Dict[str, list], **kwargs: Any):
+        super().__init__(**kwargs)
+        self.files = files
+
+    def listdir(self, sftp_folder: str):
+        return self.files[sftp_folder]
+
+    def get(self, file: str, localpath: str):
+        pass
+
+    def remove(self, file: str):
+        pass
+
+    def close(self):
+        pass
 
 
 class TestJstor(ObservatoryTestCase):
@@ -41,16 +54,19 @@ class TestJstor(ObservatoryTestCase):
         :param args: arguments.
         :param kwargs: keyword arguments.
         """
-
         super(TestJstor, self).__init__(*args, **kwargs)
         self.project_id = os.getenv('TESTS_GOOGLE_CLOUD_PROJECT_ID')
         self.data_location = os.getenv('TESTS_DATA_LOCATION')
-        self.list_releases_path = test_fixtures_path('vcr_cassettes', 'jstor', 'list_releases.yaml')
-        self.list_releases_path1 = test_fixtures_path('vcr_cassettes', 'jstor', 'list_releases1.yaml')
-        self.list_releases_path2 = test_fixtures_path('vcr_cassettes', 'jstor', 'list_releases2.yaml')
-
-    def download_path(self, report_type: str) -> str:
-        return test_fixtures_path('telescopes', 'jstor', f'{report_type}.tsv')
+        self.sftp_service_conn = os.getenv('TESTS_SFTP_SERVICE_CONN')
+        self.release_date = pendulum.parse('20210311')
+        self.download_hashes = {
+            'jstor_country.tsv': '23462a6264198b75faa56584bae0bc3d',
+            'jstor_institution.tsv': '191273d0b5230201125a6296eff336f6'
+        }
+        self.transform_hashes = {
+            'jstor_country.jsonl.gz': 'd40dfc62',
+            'jstor_institution.jsonl.gz': '1939ee8a'
+        }
 
     def test_dag_structure(self):
         """ Test that the Jstor DAG has the correct structure.
@@ -84,7 +100,6 @@ class TestJstor(ObservatoryTestCase):
 
         :return: None.
         """
-
         # Setup Observatory environment
         env = ObservatoryEnvironment(self.project_id, self.data_location)
         dataset_id = env.add_dataset()
@@ -92,83 +107,85 @@ class TestJstor(ObservatoryTestCase):
         # Setup Telescope
         execution_date = pendulum.datetime(year=2020, month=11, day=1)
         telescope = JstorTelescope(dataset_id=dataset_id)
+        telescope.sftp_folder = '/unittests/jstor'
         dag = telescope.make_dag()
 
         # Create the Observatory environment and run tests
         with env.create():
             # add gmail connection
-            conn = Connection(conn_id=AirflowConns.GMAIL_API)
-            conn.parse_from_uri('gcp://login:passwd@host/extra?credentials=credentials'
-)
+            conn = Connection(conn_id=AirflowConns.SFTP_SERVICE)
+            conn.parse_from_uri(self.sftp_service_conn)
             env.add_connection(conn)
-
-            # Release settings
-            release_date = pendulum.datetime(year=2021, month=3, day=5, hour=1, minute=34, second=32)
-            release = SnapshotRelease(telescope.dag_id, release_date)
-
-            release_id = release.release_id
-            download_folder = release.download_folder
-            extract_folder = release.extract_folder
-            transform_folder = release.transform_folder
 
             # Test that all dependencies are specified: no error should be thrown
             env.run_task(dag, telescope.check_dependencies.__name__, execution_date)
 
-            # Test list releases task
-            with vcr.use_cassette(self.list_releases_path):
-                ti = env.run_task(dag, telescope.list_releases.__name__, execution_date)
+            # TODO
+            # # Test list releases task when no files are available
+            # with patch('pysftp.Connection') as mock_pysftp:
+            #     mock_pysftp.return_value = MockSftpService({telescope.sftp_folder: ['']})
+            #     ti = env.run_task(dag, telescope.list_releases.__name__, execution_date)
 
-            pulled_list_releases = ti.xcom_pull(key=JstorTelescope.RELEASE_INFO,
-                                                task_ids=telescope.list_releases.__name__, include_prior_dates=False)
-            self.assertIsInstance(pulled_list_releases, dict)
-            for release_date, reports_info in pulled_list_releases.items():
+            # Test list releases task with files available
+            ti = env.run_task(dag, telescope.list_releases.__name__, execution_date)
+            reports_info = ti.xcom_pull(key=JstorTelescope.RELEASE_INFO, task_ids=telescope.list_releases.__name__,
+                                        include_prior_dates=False)
+            self.assertIsInstance(reports_info, dict)
+            releases = []
+            for release_date, sftp_files in reports_info.items():
                 self.assertIsInstance(release_date, Pendulum)
-                for report in reports_info:
-                    self.assertIn(report['type'], ['country', 'institution'])
-                    self.assertTrue(report['url'].startswith('https://www.jstor.org/admin/reports/download/'))
-                    self.assertIsInstance(report['id'], str)
+                self.assertEqual(self.release_date, release_date)
+                self.assertEqual([os.path.join(telescope.sftp_folder, 'PUB_anupress_PUBBCU_20210311.tsv'),
+                                  os.path.join(telescope.sftp_folder, 'PUB_anupress_PUBBIU_20210311.tsv')], sftp_files)
+
+                # use release info for other tasks
+                release = JstorRelease(telescope.dag_id, release_date, sftp_files)
+                releases.append(release)
 
             # Test download task
             env.run_task(dag, telescope.download.__name__, execution_date)
+            for release in releases:
+                self.assertEqual(2, len(release.download_files))
 
-            # with httpretty.enabled():
-            #     for release_date, reports_info in pulled_list_releases.items():
-            #         for report in reports_info:
-            #             url = report['url']
-            #             report_type = report['type']
-            #             self.setup_mock_file_download(url, self.download_path(report_type))
-            #             env.run_task(dag, telescope.download.__name__, execution_date)
+                for file in release.download_files:
+                    expected_file_hash = self.download_hashes[os.path.basename(file)]
+                    self.assert_file_integrity(file, expected_file_hash, 'md5')
 
-            # download_file_path = os.path.join(download_folder, f'{telescope.dag_id}.zip')
-            # expected_file_hash = _hash_file(self.all_countries_path, algorithm='md5')
-            # self.assert_file_integrity(download_file_path, expected_file_hash, 'md5')
-            #
-            # # Test that file uploaded
-            # env.run_task(dag, telescope.upload_downloaded.__name__, execution_date)
-            # self.assert_blob_integrity(env.download_bucket, blob_name(download_file_path), download_file_path)
-            #
-            # # Test that file extracted
-            # env.run_task(dag, telescope.extract.__name__, execution_date)
-            # extracted_file_path = os.path.join(extract_folder, 'allCountries.txt')
-            # expected_file_hash = 'de1bf005df4840d16faf598999d72051'
-            # self.assert_file_integrity(extracted_file_path, expected_file_hash, 'md5')
-            #
-            # # Test that file transformed
-            # env.run_task(dag, telescope.transform.__name__, execution_date)
-            # transformed_file_path = os.path.join(transform_folder, f'{telescope.dag_id}.csv.gz')
-            # expected_file_hash = '26c14e16'
-            # self.assert_file_integrity(transformed_file_path, expected_file_hash, 'gzip_crc')
-            #
-            # # Test that transformed file uploaded
-            # env.run_task(dag, telescope.upload_transformed.__name__, execution_date)
-            # self.assert_blob_integrity(env.transform_bucket, blob_name(transformed_file_path), transformed_file_path)
-            #
-            # # Test that data loaded into BigQuery
-            # env.run_task(dag, telescope.bq_load.__name__, execution_date)
-            # table_id = f'{self.project_id}.{dataset_id}.{bigquery_partitioned_table_id(telescope.dag_id, release_date)}'
-            # expected_rows = 50
-            # self.assert_table_integrity(table_id, expected_rows)
-            #
-            # # Test that all telescope data deleted
-            # env.run_task(dag, telescope.cleanup.__name__, execution_date)
-            # self.assert_cleanup(download_folder, extract_folder, transform_folder)
+            # Test that file uploaded
+            env.run_task(dag, telescope.upload_downloaded.__name__, execution_date)
+            for release in releases:
+                for file in release.download_files:
+                    self.assert_blob_integrity(env.download_bucket, blob_name(file), file)
+
+            # Test that file transformed
+            env.run_task(dag, telescope.transform.__name__, execution_date)
+            for release in releases:
+                for file in release.transform_files:
+                    transformed_file_hash = self.transform_hashes[os.path.basename(file)]
+                    self.assert_file_integrity(file, transformed_file_hash, 'gzip_crc')
+
+            # Test that transformed file uploaded
+            env.run_task(dag, telescope.upload_transformed.__name__, execution_date)
+            for release in releases:
+                for file in release.transform_files:
+                    self.assert_blob_integrity(env.transform_bucket, blob_name(file), file)
+
+            # Test that data loaded into BigQuery
+            env.run_task(dag, telescope.bq_load.__name__, execution_date)
+            for release in releases:
+                for file in release.transform_files:
+                    table_id, _ = table_ids_from_path(file)
+                    table_id = f'{self.project_id}.{telescope.dataset_id}.{table_id}{release_date.strftime("%Y%m%d")}'
+                    expected_rows = 9
+                    self.assert_table_integrity(table_id, expected_rows)
+
+            # Test that all telescope data deleted
+            download_folder, extract_folder, transform_folder = release.download_folder, release.extract_folder, \
+                                                                release.transform_folder
+            # mock pysftp, so unittest files are not deleted from server
+            with patch('pysftp.Connection') as mock_pysftp:
+                mock_pysftp.return_value = MockSftpService({
+                                                               telescope.sftp_folder: ['']
+                                                           })
+                env.run_task(dag, telescope.cleanup.__name__, execution_date)
+            self.assert_cleanup(download_folder, extract_folder, transform_folder)
