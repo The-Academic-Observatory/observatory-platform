d9830e41c5821e1e7c3081a7e1f99022a50aabf9
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-03-12 08:31:41 +0800

    Download reports from gmail (not working, bot detection prevents download from url)

--- observatory-dags/observatory/dags/telescopes/jstor.py
+++ observatory-dags/observatory/dags/telescopes/jstor.py
@@ -23,7 +23,7 @@ import os
 import re
 from datetime import datetime
 from shutil import copyfile
-from typing import List
+from typing import Dict, List
 from zipfile import BadZipFile, ZipFile
 
 import pendulum
@@ -48,7 +48,15 @@ from google.oauth2.credentials import Credentials
 import base64
 from bs4 import BeautifulSoup, SoupStrainer
 import csv
-
+from typing import Tuple
+from observatory.platform.utils.url_utils import get_ao_user_agent
+from selenium import webdriver
+from observatory.platform.utils.url_utils import get_ao_user_agent
+import chromedriver_binary # Adds chromedriver binary to path
+import shutil
+from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+import requests
+import time
 
 class JstorRelease(SnapshotRelease):
 
@@ -76,16 +84,59 @@ class JstorRelease(SnapshotRelease):
 
         :return: None.
         """
+        # logger = logging.getLogger(__name__).getEffectiveLevel()
+        # req_proxy = RequestProxy(log_level=logger)
+        # error = True
+        # while error is True:
+        #     proxy = req_proxy.randomize_proxy()
+        #     proxy_address = proxy.get_address()
+        #     try:
+        #         requests.get('http://example.com', proxies={'http': proxy_address}, timeout=5)
+        #         error = False
+        #     except (requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+        #         error = True
+
+        # set-up webdriver options
+        options = webdriver.ChromeOptions()
+        # options.headless = True
+        user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"
+        options.add_argument(f'user-agent={user_agent}')
+        # options.add_argument(f'--proxy-server={proxy_address}')
+
+        tmp_download_dir = os.path.join(self.download_folder, 'tmp')
+        if not os.path.exists(tmp_download_dir):
+            os.mkdir(tmp_download_dir)
+        params = {
+            'behavior': 'allow',
+            'downloadPath': tmp_download_dir
+        }
+
+        # create driver
+        driver = webdriver.Chrome(chrome_options=options)
+        # driver.desired_capabilities['proxy'] = {
+        #     "httpProxy": proxy_address,
+        #     "ftpProxy": proxy_address,
+        #     "sslProxy": proxy_address,
+        #     "proxyType": "MANUAL",
+        #
+        # }
+        # webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
+        driver.execute_cdp_cmd('Page.setDownloadBehavior', params)
 
         for report_info in self.reports_info:
             report_type = report_info['type']
             url = report_info['url']
 
-            download_path = self.download_path(report_type)
             # Download
+            download_path = self.download_path(report_type)
             logging.info(f"Downloading file: {download_path}, url: {url}")
-            file_path, updated = get_file(download_path, url, cache_subdir='',
-                                          cache_dir=self.download_folder)
+            time.sleep(5)
+            driver.get(url)
+            time.sleep(5)
+
+            # Move/rename file
+            tmp_file_name = os.path.join(tmp_download_dir, os.listdir(tmp_download_dir)[0])
+            shutil.move(tmp_file_name, self.download_path(report_type))
 
     def transform(self):
         """ Transform a Jstor release into json lines format and gzip the result.
@@ -101,12 +152,21 @@ class JstorRelease(SnapshotRelease):
             report_type = 'country' if 'country' in file else 'institution'
             list_to_jsonl_gz(self.transform_path(report_type), results)
 
+    def cleanup(self) -> None:
+        super().cleanup()
 
-def create_gmail_service():
+        service = create_gmail_service()
+        for report in self.reports_info:
+            message_id = report['id']
+            body = {'addLabelIds': [JstorTelescope.PROCESSED_LABEL_ID]}
+            service.users().messages().modify(userId='me', id=message_id, body=body).execute()
+
+
+def create_gmail_service() -> Resource:
     gmail_api_conn = BaseHook.get_connection(AirflowConns.GMAIL_API)
-    json_credentials = json.loads(gmail_api_conn.extra_dejson['extra__google_cloud_platform__keyfile_dict'])
+    json_credentials = json.loads(gmail_api_conn.extra_dejson['credentials'])
 
-    scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+    scopes = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify']
     creds = Credentials.from_authorized_user_info(json_credentials, scopes=scopes)
 
     service = build('gmail', 'v1', credentials=creds, cache_discovery=False)
@@ -114,38 +174,67 @@ def create_gmail_service():
     return service
 
 
-def list_available_releases(service, current_publisher: str) -> dict:
+def already_processed(message, processed_label: str) -> bool:
+    label_ids = message['labelIds']
+    for label in label_ids:
+        if label == processed_label:
+            return True
+
+
+def check_report_name(message, current_publisher: str) -> Tuple[str, str, bool]:
+    report_name = message['snippet'].split('&quot;')[1]
+    publisher = report_name.split(' ')[0]
+    report_type = report_name.split(' ')[1].lower()
+    if publisher not in [current_publisher] or report_type not in ['country', 'institution']:
+        logging.info(f"Found report with invalid name: {report_name}. '{publisher}' is not in "
+                     f"{[current_publisher]} and/or {report_type} is not in {['country', 'institution']}.")
+        return None, None, False
+    if publisher != current_publisher:
+        return None, None, False
+    return publisher, report_type, True
+
+
+def list_available_releases(service: Resource, current_publisher: str, processed_label: str) -> Dict[Pendulum,
+                                                                                                     List[dict]]:
     available_releases = {}
     # Call the Gmail API
     results = service.users().messages().list(userId='me', q='subject:"JSTOR Publisher Report Available"').execute()
     for message_info in results['messages']:
-        id = message_info['id']
+        message_id = message_info['id']
+        message = service.users().messages().get(userId='me', id=message_id).execute()
 
-        message = service.users().messages().get(userId='me', id=id).execute()
-
-        publisher = message['snippet'].split('&quot;')[1].split(' ')[0]
-        if publisher != current_publisher:
+        # read label
+        if already_processed(message, processed_label):
             continue
 
-        report_type = message['snippet'].split('&quot;')[1].split(' ')[1].lower()
+        # get publisher and report type
+        publisher, report_type, valid = check_report_name(message, current_publisher)
+        if not valid:
+            continue
 
+        # get release date
         timestamp = int(message['internalDate'][:-3])
-        release_date = pendulum.from_timestamp(timestamp)
+        # pendulum object at date level granularity
+        release_date = pendulum.parse(str(pendulum.from_timestamp(timestamp).date()))
 
+        # get download url
         download_url = None
         message_data = base64.urlsafe_b64decode(message['payload']['body']['data'])
         for link in BeautifulSoup(message_data, 'html.parser', parse_only=SoupStrainer('a')):
             if link.text == 'Download Completed Report':
                 download_url = link['href']
-                continue
+                break
         if download_url is None:
             raise AirflowException(f"Can't find download link for report of {publisher} at {release_date} in e-mail.")
 
+        # add report info
         try:
-            available_releases[release_date].append({'type': report_type, 'url': download_url})
+            available_releases[release_date].append({'type': report_type, 'url': download_url, 'id': message_id})
         except KeyError:
-            available_releases[release_date] = [{'type': report_type, 'url': download_url}]
+            available_releases[release_date] = [{'type': report_type, 'url': download_url, 'id': message_id}]
 
+        logging.info(f'Processing report. Publisher: {publisher}, report type: {report_type}, release date: '
+                     f'{release_date}, url: {download_url}, message id: {message_id}.')
     return available_releases
 
 
@@ -157,6 +246,7 @@ class JstorTelescope(SnapshotTelescope):
     """
 
     DAG_ID = 'jstor'
+    PROCESSED_LABEL_ID = 'processed_report'
 
     def __init__(self, dag_id: str = DAG_ID, start_date: datetime = datetime(2015, 9, 1),
                  schedule_interval: str = '@monthly', dataset_id: str = 'jstor',
@@ -225,7 +315,7 @@ class JstorTelescope(SnapshotTelescope):
         """
 
         service = create_gmail_service()
-        available_releases = list_available_releases(service, self.publisher)
+        available_releases = list_available_releases(service, self.publisher, self.PROCESSED_LABEL_ID)
 
         continue_dag = len(available_releases)
         if continue_dag:
