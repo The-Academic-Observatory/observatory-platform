c66929aeae73d459c870efeeb0a2bc1fa6ff5f58
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-03-26 10:17:13 +0800

    Add unit tests

--- tests/observatory/dags/telescopes/test_jstor.py
+++ tests/observatory/dags/telescopes/test_jstor.py
@@ -14,35 +14,81 @@
 
 # Author: Aniek Roelofs
 
+import base64
 import os
-from typing import Any, Dict
+from typing import Any
 from unittest.mock import Mock, patch
 
+import httpretty
 import pendulum
 from airflow.models.connection import Connection
 from observatory.dags.telescopes.jstor import (JstorRelease, JstorTelescope)
 from observatory.platform.utils.airflow_utils import AirflowConns
 from observatory.platform.utils.template_utils import blob_name, table_ids_from_path
 from observatory.platform.utils.test_utils import ObservatoryEnvironment, ObservatoryTestCase
-from pendulum import Pendulum
+
+from tests.observatory.test_utils import test_fixtures_path
 
 
-class MockSftpService(Mock):
-    def __init__(self, files: Dict[str, list], **kwargs: Any):
+class MockGmailService(Mock):
+    """
+    Mock the gmail api service
+    """
+    def __init__(self, messages_dict: dict, message: dict, label_dict: dict, **kwargs: Any):
         super().__init__(**kwargs)
-        self.files = files
+        self.messages_dict = messages_dict
+        self.message = message
+        self.label_dict = label_dict
 
-    def listdir(self, sftp_folder: str):
-        return self.files[sftp_folder]
+    def users(self):
+        return Users(self.messages_dict, self.message, self.label_dict)
 
-    def get(self, file: str, localpath: str):
-        pass
 
-    def remove(self, file: str):
-        pass
+class Users:
+    def __init__(self, messages_dict: dict, message: dict, label_dict: dict):
+        self.messages_dict = messages_dict
+        self.message = message
+        self.label_dict = label_dict
 
-    def close(self):
-        pass
+    def messages(self):
+        return Messages(self.messages_dict, self.message)
+
+    def labels(self):
+        return Labels(self.label_dict)
+
+
+class Labels:
+    def __init__(self, label_dict: dict):
+        self.label_dict = label_dict
+
+    def list(self, userId: str):
+        return Request(self.label_dict)
+
+    def create(self):
+        return Request()
+
+
+class Messages:
+    def __init__(self, messages_dict: dict, message: dict):
+        self.messages_dict = messages_dict
+        self.message = message
+
+    def list(self, userId: str, q: str):
+        return Request(self.messages_dict)
+
+    def get(self, userId: str, id: str):
+        return Request(self.message[id])
+
+    def modify(self, userId: str, id: str, body: dict):
+        return Request()
+
+
+class Request:
+    def __init__(self, result=None):
+        self.result = result
+
+    def execute(self):
+        return self.result
 
 
 class TestJstor(ObservatoryTestCase):
@@ -57,16 +103,20 @@ class TestJstor(ObservatoryTestCase):
         super(TestJstor, self).__init__(*args, **kwargs)
         self.project_id = os.getenv('TESTS_GOOGLE_CLOUD_PROJECT_ID')
         self.data_location = os.getenv('TESTS_DATA_LOCATION')
-        self.sftp_service_conn = os.getenv('TESTS_SFTP_SERVICE_CONN')
-        self.release_date = pendulum.parse('20210311')
-        self.download_hashes = {
-            'jstor_country.tsv': '23462a6264198b75faa56584bae0bc3d',
-            'jstor_institution.tsv': '191273d0b5230201125a6296eff336f6'
+        self.gmail_api_conn = 'google-cloud-platform://?token=123&refresh_token=123' \
+                              '&client_id=123.apps.googleusercontent.com&client_secret=123'
+
+        self.release_date = pendulum.parse('20210301')
+
+        self.country_report_path = test_fixtures_path('telescopes', 'jstor', 'country_20210301.tsv')
+        self.country_report_url = 'https://www.jstor.org/admin/reports/download/12345'
+        self.headers = {
+            'Content-Disposition': f'attachment; filename=PUB_anupress_PUBBCU_'
+                                   f'{self.release_date.strftime("%Y%m%d")}.tsv'
         }
-        self.transform_hashes = {
-            'jstor_country.jsonl.gz': 'd40dfc62',
-            'jstor_institution.jsonl.gz': '1939ee8a'
-        }
+
+        self.download_hash = '9c5eff69085457758e3743d229ec46a1'
+        self.transform_hash = 'e772a805'
 
     def test_dag_structure(self):
         """ Test that the Jstor DAG has the correct structure.
@@ -95,11 +145,48 @@ class TestJstor(ObservatoryTestCase):
         with ObservatoryEnvironment().create():
             self.assert_dag_load('jstor')
 
-    def test_telescope(self):
+    @patch('googleapiclient.discovery.Resource')
+    def test_telescope(self, mock_gmail_service):
         """ Test the Jstor telescope end to end.
 
         :return: None.
         """
+        mock_gmail_service.return_value = MockGmailService(messages_dict={
+            'messages': [{
+                             'id': '1785b19cf37d3044',
+                             'threadId': '1785b19cf37d3044'
+                         }]
+        }, message={
+            '1785b19cf37d3044': {
+                'id': '1785b19cf37d3044',
+                'threadId': '1785b19cf37d3044',
+                'labelIds': ['IMPORTANT', 'Label_3', 'CATEGORY_UPDATES', 'INBOX'],
+                'snippet': 'JSTOR JSTOR Usage Reports Report Complete Twitter Facebook Tumblr Dear OAEBU Service '
+                           'Account, Your usage report &quot;Book Usage by Country&quot; is now available to '
+                           'download. Download Completed Report',
+                'payload': {
+                    'body': {
+                        'data': base64.urlsafe_b64encode(
+                            f'<a href="{self.country_report_url}">Download Completed Report</a>'.encode())
+                    }
+                }
+            }
+        }, label_dict={
+            'labels': [{
+                           'id': 'CHAT',
+                           'name': 'CHAT',
+                           'messageListVisibility': 'hide',
+                           'labelListVisibility': 'labelHide',
+                           'type': 'system'
+                       }, {
+                           'id': 'Label_1',
+                           'name': JstorTelescope.PROCESSED_LABEL_NAME,
+                           'messageListVisibility': 'show',
+                           'labelListVisibility': 'labelShow',
+                           'type': 'user'
+                       }]
+        })
+
         # Setup Observatory environment
         env = ObservatoryEnvironment(self.project_id, self.data_location)
         dataset_id = env.add_dataset()
@@ -107,48 +194,50 @@ class TestJstor(ObservatoryTestCase):
         # Setup Telescope
         execution_date = pendulum.datetime(year=2020, month=11, day=1)
         telescope = JstorTelescope(dataset_id=dataset_id)
-        telescope.sftp_folder = '/unittests/jstor'
         dag = telescope.make_dag()
 
         # Create the Observatory environment and run tests
         with env.create():
             # add gmail connection
-            conn = Connection(conn_id=AirflowConns.SFTP_SERVICE)
-            conn.parse_from_uri(self.sftp_service_conn)
+            conn = Connection(conn_id=AirflowConns.GMAIL_API)
+            conn.parse_from_uri(self.gmail_api_conn)
             env.add_connection(conn)
 
             # Test that all dependencies are specified: no error should be thrown
             env.run_task(dag, telescope.check_dependencies.__name__, execution_date)
 
-            # TODO
-            # # Test list releases task when no files are available
-            # with patch('pysftp.Connection') as mock_pysftp:
-            #     mock_pysftp.return_value = MockSftpService({telescope.sftp_folder: ['']})
-            #     ti = env.run_task(dag, telescope.list_releases.__name__, execution_date)
-
             # Test list releases task with files available
-            ti = env.run_task(dag, telescope.list_releases.__name__, execution_date)
-            reports_info = ti.xcom_pull(key=JstorTelescope.RELEASE_INFO, task_ids=telescope.list_releases.__name__,
-                                        include_prior_dates=False)
-            self.assertIsInstance(reports_info, dict)
+            with httpretty.enabled():
+                self.setup_mock_file_download(self.country_report_url, self.country_report_path, headers=self.headers,
+                                              method=httpretty.HEAD)
+                ti = env.run_task(dag, telescope.list_releases.__name__, execution_date)
+            available_releases = ti.xcom_pull(key=JstorTelescope.RELEASE_INFO,
+                                              task_ids=telescope.list_releases.__name__, include_prior_dates=False)
+            self.assertIsInstance(available_releases, dict)
             releases = []
-            for release_date, sftp_files in reports_info.items():
-                self.assertIsInstance(release_date, Pendulum)
+            for release_date, reports_info in available_releases.items():
                 self.assertEqual(self.release_date, release_date)
-                self.assertEqual([os.path.join(telescope.sftp_folder, 'PUB_anupress_PUBBCU_20210311.tsv'),
-                                  os.path.join(telescope.sftp_folder, 'PUB_anupress_PUBBIU_20210311.tsv')], sftp_files)
+                self.assertIsInstance(reports_info, list)
+                self.assertEqual(1, len(reports_info))
+                for report in reports_info:
+                    self.assertDictEqual({
+                                             'type': 'country',
+                                             'url': 'https://www.jstor.org/admin/reports/download/12345',
+                                             'id': '1785b19cf37d3044'
+                                         }, report)
 
                 # use release info for other tasks
-                release = JstorRelease(telescope.dag_id, release_date, sftp_files)
+                release = JstorRelease(telescope.dag_id, release_date, reports_info)
                 releases.append(release)
 
             # Test download task
-            env.run_task(dag, telescope.download.__name__, execution_date)
+            with httpretty.enabled():
+                self.setup_mock_file_download(self.country_report_url, self.country_report_path, headers=self.headers)
+                env.run_task(dag, telescope.download.__name__, execution_date)
             for release in releases:
-                self.assertEqual(2, len(release.download_files))
-
+                self.assertEqual(1, len(release.download_files))
                 for file in release.download_files:
-                    expected_file_hash = self.download_hashes[os.path.basename(file)]
+                    expected_file_hash = self.download_hash
                     self.assert_file_integrity(file, expected_file_hash, 'md5')
 
             # Test that file uploaded
@@ -160,8 +249,9 @@ class TestJstor(ObservatoryTestCase):
             # Test that file transformed
             env.run_task(dag, telescope.transform.__name__, execution_date)
             for release in releases:
+                self.assertEqual(1, len(release.transform_files))
                 for file in release.transform_files:
-                    transformed_file_hash = self.transform_hashes[os.path.basename(file)]
+                    transformed_file_hash = self.transform_hash
                     self.assert_file_integrity(file, transformed_file_hash, 'gzip_crc')
 
             # Test that transformed file uploaded
@@ -176,16 +266,11 @@ class TestJstor(ObservatoryTestCase):
                 for file in release.transform_files:
                     table_id, _ = table_ids_from_path(file)
                     table_id = f'{self.project_id}.{telescope.dataset_id}.{table_id}{release_date.strftime("%Y%m%d")}'
-                    expected_rows = 9
+                    expected_rows = 10
                     self.assert_table_integrity(table_id, expected_rows)
 
             # Test that all telescope data deleted
             download_folder, extract_folder, transform_folder = release.download_folder, release.extract_folder, \
                                                                 release.transform_folder
-            # mock pysftp, so unittest files are not deleted from server
-            with patch('pysftp.Connection') as mock_pysftp:
-                mock_pysftp.return_value = MockSftpService({
-                                                               telescope.sftp_folder: ['']
-                                                           })
-                env.run_task(dag, telescope.cleanup.__name__, execution_date)
+            env.run_task(dag, telescope.cleanup.__name__, execution_date)
             self.assert_cleanup(download_folder, extract_folder, transform_folder)
