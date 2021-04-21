075047ffca3eede5b91f0f5f31fb65d24c9ddf3b
tuanchien <tuanchien@users.noreply.github.com>
2021-04-21 15:35:03 +1200

    Bugfix/find schema (#401)
    * find_schema:  fix table id prefixing another table
    
    - handle case when a table id is a prefix of another table id, e.g., tableone, tableonenew
    - simplify file naming convention of schemas
    
    * find_schema: add tests for versioned schema

--- tests/observatory/platform/utils/test_config_utils.py
+++ tests/observatory/platform/utils/test_config_utils.py
@@ -20,37 +20,41 @@ import unittest
 from unittest.mock import patch
 
 import pendulum
-from click.testing import CliRunner
-
 import tests.observatory.platform.utils as platform_utils_tests
-from observatory.platform.utils.config_utils import (find_schema,
-                                                     module_file_path,
-                                                     observatory_home,
-                                                     terraform_credentials_path)
-from observatory.platform.utils.template_utils import SubFolder, telescope_path, test_data_path
-from observatory.platform.utils.template_utils import reset_variables
+from click.testing import CliRunner
+from observatory.platform.utils.config_utils import (
+    find_schema,
+    module_file_path,
+    observatory_home,
+    terraform_credentials_path,
+)
+from observatory.platform.utils.template_utils import (
+    SubFolder,
+    reset_variables,
+    telescope_path,
+    test_data_path,
+)
 from tests.observatory.test_utils import test_fixtures_path
 
 
 class TestConfigUtils(unittest.TestCase):
-
     def test_module_file_path(self):
         # Go back one step (the default)
         expected_path = str(pathlib.Path(*pathlib.Path(platform_utils_tests.__file__).resolve().parts[:-1]).resolve())
-        actual_path = module_file_path('tests.observatory.platform.utils', nav_back_steps=-1)
+        actual_path = module_file_path("tests.observatory.platform.utils", nav_back_steps=-1)
         self.assertEqual(expected_path, actual_path)
 
         # Go back two steps
         expected_path = str(pathlib.Path(*pathlib.Path(platform_utils_tests.__file__).resolve().parts[:-2]).resolve())
-        actual_path = module_file_path('tests.observatory.platform.utils', nav_back_steps=-2)
+        actual_path = module_file_path("tests.observatory.platform.utils", nav_back_steps=-2)
         self.assertEqual(expected_path, actual_path)
 
-    @patch('observatory.platform.utils.config_utils.pathlib.Path.home')
+    @patch("observatory.platform.utils.config_utils.pathlib.Path.home")
     def test_observatory_home(self, home_mock):
         runner = CliRunner()
         with runner.isolated_filesystem():
             # Create home path and mock getting home path
-            home_path = 'user-home'
+            home_path = "user-home"
             os.makedirs(home_path, exist_ok=True)
             home_mock.return_value = home_path
 
@@ -58,23 +62,23 @@ class TestConfigUtils(unittest.TestCase):
                 # Test that observatory home works
                 path = observatory_home()
                 self.assertTrue(os.path.exists(path))
-                self.assertEqual(f'{home_path}/.observatory', path)
+                self.assertEqual(f"{home_path}/.observatory", path)
 
                 # Test that subdirectories are created
-                path = observatory_home('subfolder')
+                path = observatory_home("subfolder")
                 self.assertTrue(os.path.exists(path))
-                self.assertEqual(f'{home_path}/.observatory/subfolder', path)
+                self.assertEqual(f"{home_path}/.observatory/subfolder", path)
 
     def test_terraform_credentials_path(self):
-        expected_path = os.path.expanduser('~/.terraform.d/credentials.tfrc.json')
+        expected_path = os.path.expanduser("~/.terraform.d/credentials.tfrc.json")
         actual_path = terraform_credentials_path()
         self.assertEqual(expected_path, actual_path)
 
     def test_find_schema(self):
-        schemas_path = os.path.join(test_fixtures_path(), 'telescopes')
+        schemas_path = os.path.join(test_fixtures_path(), "telescopes")
 
         # Tests that don't use a prefix
-        table_name = 'grid'
+        table_name = "grid"
 
         # 2020-09-21
         release_date = pendulum.datetime(year=2015, month=9, day=21)
@@ -82,7 +86,7 @@ class TestConfigUtils(unittest.TestCase):
         self.assertIsNone(result)
 
         # 2020-09-22
-        expected_schema = 'grid_2015-09-22.json'
+        expected_schema = "grid_2015-09-22.json"
         release_date = pendulum.datetime(year=2015, month=9, day=22)
         result = find_schema(schemas_path, table_name, release_date)
         self.assertIsNotNone(result)
@@ -95,7 +99,7 @@ class TestConfigUtils(unittest.TestCase):
         self.assertTrue(result.endswith(expected_schema))
 
         # 2020-04-28
-        expected_schema = 'grid_2016-04-28.json'
+        expected_schema = "grid_2016-04-28.json"
         release_date = pendulum.datetime(year=2016, month=4, day=28)
         result = find_schema(schemas_path, table_name, release_date)
         self.assertIsNotNone(result)
@@ -108,8 +112,8 @@ class TestConfigUtils(unittest.TestCase):
         self.assertTrue(result.endswith(expected_schema))
 
         # Tests that use a prefix
-        table_name = 'Papers'
-        prefix = 'Mag'
+        table_name = "Papers"
+        prefix = "Mag"
 
         # 2020-05-20
         release_date = pendulum.datetime(year=2020, month=5, day=20)
@@ -117,7 +121,7 @@ class TestConfigUtils(unittest.TestCase):
         self.assertIsNone(result)
 
         # 2020-05-21
-        expected_schema = 'MagPapers2020-05-21.json'
+        expected_schema = "MagPapers_2020-05-21.json"
         release_date = pendulum.datetime(year=2020, month=5, day=21)
         result = find_schema(schemas_path, table_name, release_date, prefix=prefix)
         self.assertIsNotNone(result)
@@ -130,7 +134,7 @@ class TestConfigUtils(unittest.TestCase):
         self.assertTrue(result.endswith(expected_schema))
 
         # 2020-06-05
-        expected_schema = 'MagPapers2020-06-05.json'
+        expected_schema = "MagPapers_2020-06-05.json"
         release_date = pendulum.datetime(year=2020, month=6, day=5)
         result = find_schema(schemas_path, table_name, release_date, prefix=prefix)
         self.assertIsNotNone(result)
@@ -142,18 +146,30 @@ class TestConfigUtils(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertTrue(result.endswith(expected_schema))
 
-    @patch('observatory.platform.utils.template_utils.AirflowVariable.get')
+        # Versioned example
+        expected_schema = "testschema_v1_2021-04-20.json"
+        release_date = pendulum.datetime(year=2021, month=4, day=20)
+        result = find_schema(schemas_path, "testschema", release_date, prefix="", ver="v1")
+        self.assertIsNotNone(result)
+        self.assertTrue(result.endswith(expected_schema))
+
+        # No schema paths
+        release_date = pendulum.datetime(year=2021, month=4, day=20)
+        result = find_schema(schemas_path, "testschema", release_date, prefix="", ver="v2")
+        self.assertIsNone(result)
+
+    @patch("observatory.platform.utils.template_utils.AirflowVariable.get")
     def test_telescope_path(self, mock_variable_get):
         runner = CliRunner()
         with runner.isolated_filesystem():
             # Mock getting home path
             reset_variables()
-            data_path = 'data'
+            data_path = "data"
             mock_variable_get.return_value = data_path
 
             # The name of the telescope to create and expected root folder
-            telescope_name = 'grid'
-            root_path = os.path.join(data_path, 'telescopes')
+            telescope_name = "grid"
+            root_path = os.path.join(data_path, "telescopes")
 
             # Create subdir
             path_downloaded = telescope_path(SubFolder.downloaded, telescope_name)
@@ -173,10 +189,10 @@ class TestConfigUtils(unittest.TestCase):
             self.assertEqual(expected, path_transformed)
             self.assertTrue(os.path.exists(path_transformed))
 
-    @patch('observatory.platform.utils.template_utils.AirflowVariable.get')
+    @patch("observatory.platform.utils.template_utils.AirflowVariable.get")
     def test_test_data_path(self, mock_variable_get):
         # Mock test data path variable
-        expected_path = '/tmp/test_data'
+        expected_path = "/tmp/test_data"
         mock_variable_get.return_value = expected_path
 
         actual_path = test_data_path()
