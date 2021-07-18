8c56b01ed3f8273dab8a708bff98ed58f852b5ec
Tuan Chien <tuanchien@users.noreply.github.com>
2021-07-19 11:22:55 +1200

    docs: move generate csv inside conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,16 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+from pathlib import Path
+
+from observatory.platform.generate_schema_csv import generate_csv
 from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
 
-project = 'Observatory Platform'
-copyright = '2020 Curtin University'
-author = 'Curtin University'
+project = "Observatory Platform"
+copyright = "2020 Curtin University"
+author = "Curtin University"
 
 # -- General configuration ---------------------------------------------------
 
@@ -28,35 +31,35 @@ author = 'Curtin University'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'pbr.sphinxext',
-    'sphinx_rtd_theme',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'autoapi.extension',
-    'recommonmark',
-    'sphinxcontrib.openapi'
+    "pbr.sphinxext",
+    "sphinx_rtd_theme",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "autoapi.extension",
+    "recommonmark",
+    "sphinxcontrib.openapi",
 ]
 
 # Auto API settings: https://github.com/readthedocs/sphinx-autoapi
-autoapi_type = 'python'
-autoapi_dirs = ['../observatory-api', '../observatory-platform', '../observatory-dags', '../observatory-reports']
+autoapi_type = "python"
+autoapi_dirs = ["../observatory-api", "../observatory-platform", "../observatory-dags", "../observatory-reports"]
 autoapi_add_toctree_entry = True
 autoapi_python_use_implicit_namespaces = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ["templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -66,8 +69,9 @@ html_static_path = []
 
 # recommonmark config, used to enable rst to be evaluated within markdown files
 def setup(app):
-    app.add_config_value('recommonmark_config', {
-        'enable_eval_rst': True,
-        'auto_toc_tree_section': 'Contents'
-    }, True)
+    app.add_config_value("recommonmark_config", {"enable_eval_rst": True, "auto_toc_tree_section": "Contents"}, True)
     app.add_transform(AutoStructify)
+
+
+generate_csv()
+# Path("_build/html").mkdir(exists_ok=True, parents=True)
