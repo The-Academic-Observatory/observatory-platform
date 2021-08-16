8e495b715440ac7b2c6c3cf0debf6d5cf653f83e
Jamie Diprose <jamie.diprose@gmail.com>
2021-08-17 10:01:05 +1200

    Remove JSTOR from Wits University Press

--- observatory-dags/observatory/dags/dags/onix_workflow.py
+++ observatory-dags/observatory/dags/dags/onix_workflow.py
@@ -119,8 +119,6 @@ def get_oaebu_partner_data(project_id, org_name):
             OaebuPartnerName.oapen_irus_uk,
         ],
         "Wits University Press": [
-            OaebuPartnerName.jstor_country,
-            OaebuPartnerName.jstor_institution,
             OaebuPartnerName.oapen_irus_uk,
         ],
     }
