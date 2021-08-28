905caad03dde0041431034f3fd2c375d83379a55
Tuan Chien <tuanchien@users.noreply.github.com>
2021-08-28 15:42:46 +1200

    INF-33: fix grid telescope and add tests

--- observatory-dags/observatory/dags/workflows/grid_telescope.py
+++ observatory-dags/observatory/dags/workflows/grid_telescope.py
@@ -28,7 +28,6 @@ import pendulum
 from airflow.exceptions import AirflowException
 from airflow.models.taskinstance import TaskInstance
 from google.cloud.bigquery import SourceFormat
-
 from observatory.dags.config import schema_folder as default_schema_folder
 from observatory.platform.utils.airflow_utils import AirflowVars
 from observatory.platform.utils.data_utils import get_file
@@ -182,7 +181,7 @@ def list_grid_records(
             date_matches = re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2})", title)
             if date_matches is None:
                 raise ValueError(f"No release date found in GRID title: {title}")
-            release_date = pendulum.parse(date_matches[0])
+            release_date = date_matches[0]
 
             try:
                 release_articles[release_date].append(article_id)
@@ -283,8 +282,7 @@ class GridTelescope(SnapshotTelescope):
         for record in records:
             article_ids = record["article_ids"]
             release_date = record["release_date"]
-
-            releases.append(GridRelease(self.dag_id, article_ids, release_date))
+            releases.append(GridRelease(self.dag_id, article_ids, pendulum.parse(release_date)))
         return releases
 
     def list_releases(self, **kwargs):
--- tests/observatory/dags/workflows/test_grid_telescope.py
+++ tests/observatory/dags/workflows/test_grid_telescope.py
@@ -12,15 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Author: James Diprose, Aniek Roelofs
+# Author: James Diprose, Aniek Roelofs, Tuan Chien
 
 import logging
 import os
 import unittest
-from unittest.mock import patch
+from pathlib import Path
+from unittest.mock import MagicMock, PropertyMock, patch
 
 import pendulum
 import vcr
+from airflow.exceptions import AirflowException
 from click.testing import CliRunner
 from observatory.dags.workflows.grid_telescope import (
     GridRelease,
@@ -28,7 +30,23 @@ from observatory.dags.workflows.grid_telescope import (
     list_grid_records,
 )
 from observatory.platform.utils.file_utils import _hash_file, gzip_file_crc
-from tests.observatory.test_utils import test_fixtures_path
+from observatory.platform.utils.test_utils import (
+    ObservatoryEnvironment,
+    ObservatoryTestCase,
+    module_file_path,
+    test_fixtures_path,
+)
+from observatory.platform.utils.workflow_utils import blob_name, table_ids_from_path
+
+
+class MockResponse:
+    def __init__(self):
+        self.text = '[{"published_date": "20210101", "id":12345, "title":"no date in here"}]'
+
+
+class MockSession:
+    def get(self, *args, **kwargs):
+        return MockResponse()
 
 
 class MockTaskInstance:
@@ -90,8 +108,8 @@ class TestGridTelescope(unittest.TestCase):
                 "start_date": pendulum.datetime(2015, 10, 11),
                 "end_date": pendulum.datetime(2015, 10, 18),
                 "records": [
-                    {"article_ids": [1570968, 1570967], "release_date": pendulum.parse("2015-10-09T00:00:00+00:00")},
-                    {"article_ids": [1553266, 1553267], "release_date": pendulum.parse("2015-09-22T00:00:00+00:00")},
+                    {"article_ids": [1570968, 1570967], "release_date": "2015-10-09"},
+                    {"article_ids": [1553266, 1553267], "release_date": "2015-09-22"},
                 ],
                 # there are 2 releases in this run, but use only 1 for testing
                 "release": GridRelease(
@@ -107,7 +125,7 @@ class TestGridTelescope(unittest.TestCase):
             self.grid_run_2020_03_27 = {
                 "start_date": pendulum.datetime(2020, 3, 20),
                 "end_date": pendulum.datetime(2020, 3, 27),
-                "records": [{"article_ids": [12022722], "release_date": pendulum.parse("2020-03-15T00:00:00+00:00")}],
+                "records": [{"article_ids": [12022722], "release_date": "2020-03-15T00:00:00+00:00"}],
                 "release": GridRelease(self.grid.dag_id, ["12022722"], pendulum.parse("2020-03-15T00:00:00+00:00")),
                 "download_path": os.path.join(self.vcr_cassettes_path, "grid_2020-03-15.yaml"),
                 "download_hash": "3d300affce1666ac50b8d945c6ca4c5a",
@@ -121,17 +139,46 @@ class TestGridTelescope(unittest.TestCase):
         logging.basicConfig()
         logging.getLogger().setLevel(logging.WARNING)
 
+    def test_ctor(self, mock_variable_get):
+        """Cover case where airflow_vars is given."""
+        telescope = GridTelescope(airflow_vars=[])
+        self.assertEqual(telescope.airflow_vars, list(["transform_bucket"]))
+
     def test_list_grid_records(self, mock_variable_get):
         """Check that list grid records returns a list of dictionaries with records in the correct format.
 
         :param mock_variable_get: Mock result of airflow's Variable.get() function
         :return: None.
         """
+
         with vcr.use_cassette(self.list_grid_records_path):
             start_date = self.grid_run_2015_10_18["start_date"]
             end_date = self.grid_run_2015_10_18["end_date"]
             records = list_grid_records(start_date, end_date, GridTelescope.GRID_DATASET_URL)
             self.assertEqual(self.grid_run_2015_10_18["records"], records)
+
+    def test_list_grid_records_bad_title(self, mock_variable_get):
+        """Check exception raised when invalid title given."""
+
+        with patch("observatory.dags.workflows.grid_telescope.retry_session", return_value=MockSession()) as _:
+            start_date = pendulum.datetime(2020, 1, 1)
+            end_date = pendulum.datetime(2022, 1, 1)
+            self.assertRaises(ValueError, list_grid_records, start_date, end_date, "")
+
+    def test_list_releases(self, mock_variable_get):
+        """Test list_releases."""
+
+        ti = MagicMock()
+        with patch("observatory.dags.workflows.grid_telescope.list_grid_records") as m_list_grid_records:
+            m_list_grid_records.return_value = []
+            telescope = GridTelescope()
+            result = telescope.list_releases(execution_date=pendulum.now(), next_execution_date=pendulum.now())
+            self.assertEqual(result, False)
+
+            m_list_grid_records.return_value = [1]
+            telescope = GridTelescope()
+            result = telescope.list_releases(execution_date=pendulum.now(), next_execution_date=pendulum.now(), ti=ti)
+            self.assertEqual(result, True)
 
     def test_make_release(self, mock_variable_get):
         """Check that make_release returns a list of GridRelease instances.
@@ -206,3 +253,172 @@ class TestGridTelescope(unittest.TestCase):
 
                     self.assertEqual(1, len(release.transform_files))
                     self.assertEqual(run["transform_crc"], gzip_file_crc(release.transform_files[0]))
+
+
+class TestGridRelease(unittest.TestCase):
+    @patch("observatory.dags.workflows.grid_telescope.GridRelease.extract_folder", new_callable=PropertyMock)
+    @patch("observatory.dags.workflows.grid_telescope.GridRelease.download_files", new_callable=PropertyMock)
+    def test_extract_not_zip_file(self, m_download_files, m_extract_folder):
+        with CliRunner().isolated_filesystem():
+            Path("file.zip").touch()
+
+            release = GridRelease(dag_id="dag", article_ids=[], release_date=pendulum.now())
+            m_download_files.return_value = ["file.zip"]
+            m_extract_folder.return_value = "."
+            release.extract()
+
+    @patch("observatory.dags.workflows.grid_telescope.GridRelease.extract_files", new_callable=PropertyMock)
+    def test_transform_multiple_extract(self, m_extract_files):
+        m_extract_files.return_value = ["1", "2"]
+
+        with CliRunner().isolated_filesystem():
+            release = GridRelease(dag_id="dag", article_ids=[], release_date=pendulum.now())
+            self.assertRaises(AirflowException, release.transform)
+
+
+class TestGridTelescopeDag(ObservatoryTestCase):
+    def __init__(self, *args, **kwargs):
+        """Constructor which sets up variables used by tests.
+
+        :param args: arguments.
+        :param kwargs: keyword arguments.
+        """
+        super().__init__(*args, **kwargs)
+
+        self.project_id = os.environ["TEST_GCP_PROJECT_ID"]
+        self.data_location = os.environ["TEST_GCP_DATA_LOCATION"]
+
+        # Paths
+        self.vcr_cassettes_path = os.path.join(test_fixtures_path(), "vcr_cassettes")
+        self.list_grid_records_path = os.path.join(self.vcr_cassettes_path, "list_grid_releases.yaml")
+        self.grid_path = os.path.join(self.vcr_cassettes_path, "grid_2015-09-22.yaml")
+
+    def setup_observatory_environment(self):
+        env = ObservatoryEnvironment(self.project_id, self.data_location)
+        self.dataset_id = env.add_dataset()
+        return env
+
+    def test_dag_structure(self):
+        """Test that the GRID DAG has the correct structure.
+
+        :return: None
+        """
+        telescope = GridTelescope()
+        dag = telescope.make_dag()
+        self.assert_dag_structure(
+            {
+                "check_dependencies": ["list_releases"],
+                "list_releases": ["download"],
+                "download": ["upload_downloaded"],
+                "upload_downloaded": ["extract"],
+                "extract": ["transform"],
+                "transform": ["upload_transformed"],
+                "upload_transformed": ["bq_load"],
+                "bq_load": ["cleanup"],
+                "cleanup": [],
+            },
+            dag,
+        )
+
+    def test_dag_load(self):
+        """Test that the GRID DAG can be loaded from a DAG bag.
+
+        :return: None
+        """
+
+        env = ObservatoryEnvironment(self.project_id, self.data_location)
+        with env.create():
+            dag_file = os.path.join(module_file_path("observatory.dags.dags"), "grid_telescope.py")
+            self.assert_dag_load("grid", dag_file)
+
+    def test_telescope(self):
+        """Test running the telescope. Functional test."""
+
+        env = self.setup_observatory_environment()
+        telescope = GridTelescope(dag_id="grid", dataset_id=self.dataset_id)
+        dag = telescope.make_dag()
+        execution_date = pendulum.datetime(year=2015, month=9, day=22)
+
+        # Create the Observatory environment and run tests
+        with env.create():
+            with env.create_dag_run(dag, execution_date):
+                # Check dependencies
+                env.run_task(telescope.check_dependencies.__name__, dag, execution_date)
+
+                # List releases
+                with patch("observatory.dags.workflows.grid_telescope.list_grid_records") as m_list_grid_records:
+                    m_list_grid_records.return_value = [
+                        {"article_ids": [1553266, 1553267], "release_date": "2015-10-09"},
+                    ]
+                    ti = env.run_task(telescope.list_releases.__name__, dag, execution_date)
+
+                # Test list releases
+                available_releases = ti.xcom_pull(
+                    key=GridTelescope.RELEASE_INFO,
+                    task_ids=telescope.list_releases.__name__,
+                    include_prior_dates=False,
+                )
+                self.assertEqual(len(available_releases), 1)
+
+                # Download
+                with vcr.use_cassette(self.grid_path):
+                    env.run_task(telescope.download.__name__, dag, execution_date)
+
+                # Test download
+                release = GridRelease(
+                    dag_id="grid",
+                    article_ids=[1553266, 1553267],
+                    release_date=pendulum.datetime(2015, 10, 9),
+                )
+                self.assertEqual(len(release.download_files), 1)
+
+                # upload_downloaded
+                env.run_task(telescope.upload_downloaded.__name__, dag, execution_date)
+
+                # Test upload_downloaded
+                for file in release.download_files:
+                    self.assert_blob_integrity(env.download_bucket, blob_name(file), file)
+
+                # extract
+                env.run_task(telescope.extract.__name__, dag, execution_date)
+
+                # Test extract
+                self.assertEqual(len(release.extract_files), 1)
+
+                # transform
+                env.run_task(telescope.transform.__name__, dag, execution_date)
+
+                # Test transform
+                self.assertEqual(len(release.transform_files), 1)
+
+                # upload_transformed
+                env.run_task(telescope.upload_transformed.__name__, dag, execution_date)
+
+                # Test upload_transformed
+                for file in release.transform_files:
+                    self.assert_blob_integrity(env.transform_bucket, blob_name(file), file)
+
+                # bq_load
+                env.run_task(telescope.bq_load.__name__, dag, execution_date)
+
+                # Test bq_load
+                # Will only check table exists rather than validate data.
+                for file in release.transform_files:
+                    table_id, _ = table_ids_from_path(file)
+                    suffix = release.release_date.format("YYYYMMDD")
+                    table_id = f"{self.project_id}.{self.dataset_id}.{table_id}{suffix}"
+                    expected_rows = 48987
+                    self.assert_table_integrity(table_id, expected_rows)
+
+                # cleanup
+                env.run_task(telescope.cleanup.__name__, dag, execution_date)
+
+                # Test cleanup
+                # Test that all telescope data deleted
+                download_folder, extract_folder, transform_folder = (
+                    release.download_folder,
+                    release.extract_folder,
+                    release.transform_folder,
+                )
+                env.run_task(telescope.cleanup.__name__, dag, execution_date)
+                self.assert_cleanup(download_folder, extract_folder, transform_folder)
