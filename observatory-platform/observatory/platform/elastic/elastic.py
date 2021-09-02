cafeff997b1d855ee3d26407732038500c4155e0
tuanchien <tuanchien@users.noreply.github.com>
2021-09-02 13:32:25 +1200

    add task to delete stale elasticsearch indices (#486)

--- observatory-platform/observatory/platform/elastic/elastic.py
+++ observatory-platform/observatory/platform/elastic/elastic.py
@@ -12,17 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Author: James Diprose
+# Author: James Diprose, Tuan Chien
 
 
 import logging
+from dataclasses import dataclass
+from enum import Enum
 from multiprocessing import cpu_count
-from typing import Dict, Iterator, List
+from typing import Dict, Iterable, Iterator, List
 
 import elasticsearch.exceptions
+import pendulum
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import parallel_bulk
-from elasticsearch.helpers import scan
+from elasticsearch.helpers import parallel_bulk, scan
 from pendulum import Date
 
 
@@ -49,6 +51,93 @@ def make_elastic_uri(schema: str, user: str, secret: str, hostname: str, port: i
     """
 
     return f"{schema}://{user}:{secret}@{hostname}:{port}"
+
+
+class KeepOrder(Enum):
+    """Specify to stale index cleaner whether to keep newest, or oldest indices."""
+
+    newest = "newest"
+    oldest = "oldest"
+
+
+@dataclass
+class KeepInfo:
+    """Information on how do delete stale Elastic indices."""
+
+    num: int = 2
+    ordering: KeepOrder = KeepOrder.newest
+
+
+class StaleIndexCleaner:
+    """Helper class to delete stale elastic search indices."""
+
+    @staticmethod
+    def has_yyyymmdd_suffix(index: str) -> bool:
+        """Check if a string ends with YYYYMMDD.
+
+        :param index: Index string to check.
+        :return: Whether it has YYYYMMDD suffix.
+        """
+
+        suffix = index[-8:]  # Last 8 chars should be YYYYMMDD
+        try:
+            pendulum.parse(suffix)
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def bucket_indices(indices: Iterable) -> dict:
+        """Organises the indices into buckets, where each index in the same bucket has the same string before the
+        YYYYMMDD suffix.
+
+        :param indices: List of indices to bucket.
+        :return: Dictionary of index buckets.
+        """
+
+        buckets = {}
+
+        for index in indices:
+            key = index[:-9]  # string before -YYYYMMDD
+            buckets[key] = buckets.get(key, list())
+            buckets[key].append(index)
+
+        return buckets
+
+    @staticmethod
+    def sort_buckets(*, buckets: dict, order: KeepOrder) -> dict:
+        """Sort the buckets into either increasing date or decreasing date sequences.
+
+        :param buckets: Buckets to sort.
+        :param order: Sorting order.
+        :return: Sorted buckets.
+        """
+
+        sorted_buckets = {}
+
+        for key, bucket in buckets.items():
+            if order == KeepOrder.oldest:
+                sorted_buckets[key] = sorted(bucket)
+            else:
+                sorted_buckets[key] = sorted(bucket, reverse=True)
+
+        return sorted_buckets
+
+    @staticmethod
+    def get_deletion_list(*, buckets: dict, num_keep: int) -> List[str]:
+        """Get a list of stale indices to delete.
+
+        :param buckets: Buckets to get indices from.
+        :param num_keep: Maximum number of indices to keep from each bucket.
+        :return: List of indices to delete.
+        """
+
+        result = list()
+
+        for bucket in buckets.values():
+            result.extend(bucket[num_keep:])
+
+        return result
 
 
 class Elastic:
@@ -93,6 +182,15 @@ class Elastic:
             self.es.indices.delete(index=index_id)
         except elasticsearch.exceptions.TransportError as e:
             logging.warning(f"Index not found: {e}")
+
+    def delete_indices(self, indices: List[str]):
+        """Delete a list of Elastic indices.
+
+        :param indices: List of Elastic indices to delete.
+        """
+
+        for index in indices:
+            self.delete_index(index)
 
     def get_alias_indexes(self, alias_id: str) -> List:
         """Return the list of indexes associated with an alias.
@@ -168,3 +266,60 @@ class Elastic:
 
         for doc in documents:
             yield {"_op_type": "index", "_index": index_id, "_source": doc}
+
+    def create_index(self, index: str, **kwargs):
+        """Create a new Elastic index.
+
+        :param index: Index to create.
+        :param **kwargs: Any additional arguments to pass to the Elasticsearch client.
+        """
+
+        self.es.indices.create(index=index, **kwargs)
+
+    def list_indices(self, index: str) -> List[str]:
+        """List all indices matching the query name. The name can contain wildcards.
+        :param index: Index name to query.
+        :return: List of matching index names.
+        """
+
+        try:
+            response = self.es.indices.get_alias(index=index)
+            indices = response.keys()
+            return list(indices)
+        except:
+            return list()
+
+    def delete_stale_indices(self, *, index: str, keep_info: KeepInfo):
+        """Keep a certain number of most recent releases (or oldest) for an index name.  The index name can
+        have wildcard if you are not matching an exact index name. For example, test-name* will match all indices with
+        the test-name prefix. The indices must end in the suffix -YYYYMMDD.  Indices not ending with -YYYYMMDD will be
+        ignored.
+
+        If more than one result is returned, the names will be clustered so that indices with the same name before the
+        -YYYYMMDD suffix are together.  Each cluster will be sorted by the date in the suffix string, and the stale
+        indices will be deleted.
+
+        :param index: Index search string to delete stale versions of.
+        :param keep_info: Metadata about how to order indices, and how many indices to keep.
+        """
+
+        # Get a list of indices matching our string.
+        indices = self.list_indices(index)
+
+        # Prune names that are too short.
+        indices = list(filter(lambda x: len(x) > 9, indices))  # Length of -YYYYMMDD is 9
+
+        # Prune names that dont have -YYYYMMDD suffix.
+        indices = list(filter(StaleIndexCleaner.has_yyyymmdd_suffix, indices))
+
+        # Put same prefixes into the same bucket. Use hash table.
+        buckets = StaleIndexCleaner.bucket_indices(indices)
+
+        # Sort each bucket. If reverse is false, sort descending, else sort ascending.
+        sorted_buckets = StaleIndexCleaner.sort_buckets(buckets=buckets, order=keep_info.ordering)
+
+        # Calculate indices to delete
+        deletion_list = StaleIndexCleaner.get_deletion_list(buckets=sorted_buckets, num_keep=keep_info.num)
+
+        # For each index in the tail, delete the index.
+        self.delete_indices(deletion_list)
