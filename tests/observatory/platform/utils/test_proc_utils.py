1afb2eab7f30bb498de8ecb60806359fe725848f
Tuan Chien <tuan@couch.science>
2021-09-23 12:02:16 +1200

    remove run_cmd, run_bash_cmd
    Those use cases can be substituted for a call to subprocess.run instead.

--- tests/observatory/platform/utils/test_proc_utils.py
+++ tests/observatory/platform/utils/test_proc_utils.py
@@ -20,16 +20,3 @@ from unittest.mock import patch
 
 from airflow.exceptions import AirflowException
 from observatory.platform.utils.proc_utils import run_bash_cmd
-
-
-class TestProcUtils(unittest.TestCase):
-    def test_run_bash_cmd(self):
-        # Run echo with stdout
-        with patch("observatory.platform.utils.proc_utils.logging.info") as m_log:
-            cmd = "echo hello"
-            run_bash_cmd(cmd)
-            self.assertEqual(m_log.call_count, 1)
-
-        # Have an error
-        cmd = "$(exit 1)"
-        self.assertRaises(AirflowException, run_bash_cmd, cmd)
