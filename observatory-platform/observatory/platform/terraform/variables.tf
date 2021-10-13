1505e6ef0d5e8ee6d4b1fd30bb49df58e5d92ed5
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-10-13 17:06:48 +0800

    INF-73: Remove api configuration from main configuration

--- observatory-platform/observatory/platform/terraform/variables.tf
+++ observatory-platform/observatory/platform/terraform/variables.tf
@@ -97,33 +97,3 @@ variable "airflow_connections" {
   description = "User defined Apache Airflow connections"
   type = map(string)
 }
-
-variable "elasticsearch" {
-    description = <<EOF
-Elasticsearch login information
-
-api_key: The elasticsearch api-key
-host: The address of the elasticsearch server
-EOF
-  type = object({
-    api_key = string
-    host = string
-  })
-}
-
-variable "api" {
-  description = <<EOF
-Settings related to the Data API
-
-name: Name of the Data API project, e.g. academic-observatory or oaebu
-//package_name: Local path to the Data API package, e.g. /path/to/academic_observatory_workflows_api
-domain_name: the custom domain name for the API, used for the google cloud endpoints service
-subdomain: can be either 'project_id' or 'environment', used to determine a prefix for the domain_name
-EOF
-  type = object({
-    name = string
-//    package = string
-    domain_name = string
-    subdomain = string
-  })
-}
