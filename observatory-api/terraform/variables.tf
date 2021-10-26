d662693da5fb9bbbd371cc29424b43103cd6aea8
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-10-26 13:49:53 +0800

    INF-73: Update api to use latest api module

--- observatory-api/terraform/variables.tf
+++ observatory-api/terraform/variables.tf
@@ -28,36 +28,42 @@ Settings related to the API
 
 name: Name of the API project, e.g. academic-observatory or oaebu
 //package_name: Local path to the Data API package, e.g. /path/to/academic_observatory_workflows_api
-domain_name: the custom domain name for the API, used for the google cloud endpoints service
-subdomain: can be either 'project_id' or 'environment', used to determine a prefix for the domain_name
+domain_name: The custom domain name for the API, used for the google cloud endpoints service
+subdomain: Can be either 'project_id' or 'environment', used to determine a prefix for the domain_name
+image_tag: The image tag that will be used for the Cloud Run backend. If the value is null, Terraform will get the image
+tag from a local file "./image_build.txt".
 EOF
   type = object({
-    name = string
-//    package = string
+    name        = string
     domain_name = string
-    subdomain = string
+    subdomain   = string
+    image_tag   = string
   })
 }
 
-variable "observatory_api" {
+variable "api_type" {
   description = <<EOF
-Settings related specifically to the Observatory API
+Setting related to the specific api type, either the data api or observatory api.
+The data api requires the observatory organization and workspace set, while the data api requires the elasticsearch
+host and api key set.
 EOF
   type = object({
+    type                     = string
     observatory_organization = string
-    observatory_workspace = string
+    observatory_workspace    = string
+    elasticsearch_api_key    = string
+    elasticsearch_host       = string
   })
-}
 
-variable "data_api" {
-  description = <<EOF
-Settings related specifically to a Data API
-
-api_key: The elasticsearch api key
-host: The address of the elasticsearch server
-EOF
-  type = object({
-    elasticsearch_api_key = string
-    elasticsearch_host = string
-  })
+  validation {
+    condition     = var.api_type.type == "data_api" || var.api_type.type == "observatory_api"
+    error_message = "The api type must either be 'data_api' or 'observatory_api'."
+  }
+  validation {
+    condition = (
+      var.api_type.type == "data_api" && var.api_type.elasticsearch_host != "" && var.api_type.elasticsearch_api_key != "" ||
+      var.api_type.type == "observatory_api" && var.api_type.observatory_organization != "" && var.api_type.observatory_workspace != ""
+    )
+    error_message = "Elasticsearch host and api key can not be empty when the api type is set to 'data_api' and observatory organization and workspace can not be empty when the api type is set to 'observatory_api'.."
+  }
 }
