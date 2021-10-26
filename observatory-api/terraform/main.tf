101ad285323d29a490d3db239e362b0b8eeb1709
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2021-10-26 13:49:53 +0800

    INF-73: Update api to use latest api module

--- observatory-api/terraform/main.tf
+++ observatory-api/terraform/main.tf
@@ -23,37 +23,61 @@ provider "google" {
   zone        = var.google_cloud.zone
 }
 
+# Get info from the observatory workspace if this is given
 data "terraform_remote_state" "observatory" {
-  count = var.observatory_api.observatory_workspace != "" ? 1 : 0
+  count   = var.api_type.type == "observatory_api" ? 1 : 0
   backend = "remote"
   config = {
-    organization = var.observatory_api.observatory_organization
+    organization = var.api_type.observatory_organization
     workspaces = {
-      name = var.observatory_api.observatory_workspace
+      name = var.api_type.observatory_workspace
     }
   }
 }
 
+# Get build info from a local file if no build_info is passed on
+data "local_file" "image_tag" {
+  count    = var.api.image_tag == "" ? 1 : 0
+  filename = "./image_build.txt"
+}
+
 locals {
-  vpc_connector_name = try(data.terraform_remote_state.observatory[0].outputs.vpc_connector_name, null)
-  observatory_db_uri = try(data.terraform_remote_state.observatory[0].outputs.observatory_db_uri, null)
-  elasticsearch_host = var.data_api.elasticsearch_host != "" ? var.data_api.elasticsearch_host : null
-  elasticsearch_api_key = var.data_api.elasticsearch_api_key != "" ? var.data_api.elasticsearch_api_key : null
+  # Set the build info, either from local file or from variable
+  image_tag = try(data.local_file.image_tag[0].content, var.api.image_tag)
+
+  # Set the environment variables for the Cloud Run backend
+  env_vars = (
+    var.api_type.type == "observatory_api" ?
+    tomap({
+      "OBSERVATORY_DB_URI" = data.terraform_remote_state.observatory[0].outputs.observatory_db_uri
+    }) :
+    tomap({
+      "ES_HOST"    = var.api_type.elasticsearch_host,
+      "ES_API_KEY" = var.api_type.elasticsearch_api_key,
+    })
+  )
+
+  # Set the annotations for the cloud run backend.
+  cloud_run_annotations = (
+    var.api_type.type == "observatory_api" ?
+    tomap({
+      "autoscaling.knative.dev/maxScale"        = "10"
+      "run.googleapis.com/vpc-access-egress"    = "private-ranges-only"
+      "run.googleapis.com/vpc-access-connector" = "projects/${var.google_cloud.project_id}/locations/${var.google_cloud.region}/connectors/${data.terraform_remote_state.observatory[0].outputs.vpc_connector_name}"
+    }) :
+    tomap({
+      "autoscaling.knative.dev/maxScale" = "10"
+    })
+  )
 }
 
 
 module "api" {
-  source  = "The-Academic-Observatory/api/google"
-  version = "0.0.2"
-  api = var.api
-  environment = var.environment
-  google_cloud = var.google_cloud
-  observatory_api = {
-    "vpc_connector_name": local.vpc_connector_name,
-    "observatory_db_uri": local.observatory_db_uri
-  }
-  data_api     = {
-    "elasticsearch_host" : local.elasticsearch_host,
-    "elasticsearch_api_key" : local.elasticsearch_api_key
-  }
+  source                = "The-Academic-Observatory/api/google"
+  version               = "0.0.6"
+  api                   = merge(var.api, { "image_tag" = local.image_tag })
+  environment           = var.environment
+  google_cloud          = var.google_cloud
+  env_vars              = local.env_vars
+  cloud_run_annotations = local.cloud_run_annotations
 }
