372367d222b55016f165ce1fda4ccb03568c086c
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2022-01-04 16:47:14 +0800

    INF-73: Add auth_utils unit tests

--- observatory-api/observatory/api/utils/auth_utils.py
+++ observatory-api/observatory/api/utils/auth_utils.py
@@ -1,3 +1,19 @@
+# Copyright 2022 Curtin University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Aniek Roelofs
+
 from flask import request, redirect, session
 from observatory.api.utils.exception_utils import AuthError
 from jose import jwt
@@ -7,6 +23,12 @@ from functools import wraps
 
 
 def requires_auth(f):
+    """Wrapper that will check if the session is authorized
+
+    :param f: Function be wrapped
+    :return: Redirect to root if not authorized, else wrapped function
+    """
+
     @wraps(f)
     def decorated(*args, **kwargs):
         if "jwt_payload" not in session:
@@ -18,6 +40,11 @@ def requires_auth(f):
 
 
 def set_auth_session(auth0: OAuth):
+    """Store access token in flask session
+
+    :param auth0:
+    :return:
+    """
     # Handles response from token endpoint
     token = auth0.authorize_access_token()
     # resp = auth0.get('userinfo')
@@ -43,11 +70,11 @@ def get_token_auth() -> Optional[str]:
         parts = auth_header.split()
 
         if parts[0].lower() != "bearer":
-            raise AuthError({"code": "invalid_header", "description": "Authorization header must start with" " Bearer"})
+            raise AuthError({"code": "invalid_header", "description": "Authorization header must start with Bearer"})
         elif len(parts) == 1:
             raise AuthError({"code": "invalid_header", "description": "Token not found"})
         elif len(parts) > 2:
-            raise AuthError({"code": "invalid_header", "description": "Authorization header must be" " Bearer token"})
+            raise AuthError({"code": "invalid_header", "description": "Authorization header must be 'Bearer <token>'"})
 
         token = parts[1]
     else:
