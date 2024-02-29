8451fcb53e3c01689e69696f3f3745fc806d5a62
keegansmith21 <keegansmith21@github.com>
2024-02-29 17:35:36 +0800

    Updated custom sensors

--- observatory_platform/airflow/sensors.py
+++ observatory_platform/airflow/sensors.py
@@ -16,14 +16,11 @@
 
 from __future__ import annotations
 
-import datetime
-import os
-from time import sleep
-from typing import Dict, Union, List
+from datetime import timedelta, datetime
+from typing import List
+import logging
 
-from airflow.exceptions import AirflowException
-from airflow.models import DagModel, DagRun
-from airflow.sensors.base import BaseSensorOperator
+from airflow.models import DagRun
 from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.utils.db import provide_session
 from airflow.utils.state import State
@@ -89,137 +86,74 @@ class PreviousDagRunSensor(ExternalTaskSensor):
         return super().poke(context, session=session)
 
 
-class DagRunSensor(BaseSensorOperator):
+class DagCompleteSensor(ExternalTaskSensor):
     """
-    A sensor for monitoring dag runs from other DAGs.
-    Behaviour:
-    * If the DAG ID does not exist, throw an exception.
-    * Monitors the time period [execution_date - duration, execution_date] for the DAG's dag runs.
-      - If there are no runs, then return a success state.
-      - If there are runs, look at the latest run in the time period:
-        * If the latest run is successful, return a success state.
-        * If the latest run is unsuccessful, return a retry state.
-    Like other sensors, you can set the poke_interval and timeout in seconds, to control retry
-    behaviour.
+    A sensor that awaits the completion of an external dag by default. Wait functionality can be customised by
+    providing a different execution_date_fn.
+
+    The sensor checks for completion of a dag with "external_dag_id" on the logical date returned by the
+    execution_date_fn.
     """
 
     def __init__(
         self,
-        *,
-        external_dag_id: str,
-        duration: datetime.timedelta,
-        check_exists: bool = True,
-        grace_period: datetime.timedelta = datetime.timedelta(minutes=10),
+        task_id,
+        external_dag_id,
+        mode="reschedule",
+        poke_interval=int(1200),  # Check if dag run is ready every 20 minutes
+        timeout=int(timedelta(days=1).total_seconds()),  # Sensor will fail after 1 day of waiting
+        check_existence=True,
+        # Custom date retrieval fn. Airflow expects a callable with the execution_date as an argument only.
+        execution_date_fn=None,
         **kwargs,
     ):
         """
-        :param external_dag_id: The DAG ID to monitor.
-        :param duration: Size of the window to look back from the current execution date.
-        :param check_exists: Whether to perform check for dag existence.
-        :param grace_period: If no dag run can be found, sleep for this short grace period and try again.
-        :param kwargs: Pass the rest of the parameters to the ExternalTaskSensor.
-        """
-        super().__init__(**kwargs)
-
-        self.duration = duration
-        self.external_dag_id = external_dag_id
-        self.check_exists = check_exists
-        self.grace_period = grace_period
-
-    @provide_session
-    def poke(self, context: Dict, session: scoped_session = None):
-        """Sensor interface for determining status of the sensor.
-
-        :param context: Poke context.
-        :param session: Database session.
-        :return: Sensor poke response.
+        :param task_id: the id of the sensor task to create
+        :param external_dag_id: the id of the external dag to check
+        :param mode: The mode of the scheduler. Can be reschedule or poke.
+        :param poke_interval: how often to check if the external dag run is complete
+        :param timeout: how long to check before the sensor fails
+        :param check_existence: Whether to check that the provided dag_id exists
+        :param execution_date_fn: The callable that returns a logical date to check for the provided dag_id
         """
 
-        success_state = True
-        retry_state = False
+        if not execution_date_fn:
+            execution_date_fn = lambda dt: latest_execution_timedelta(dt, external_dag_id)
 
-        if self.check_exists:
-            self.check_dag_exists(session)
-        execution_date = context["execution_date"]
-
-        date = self.get_latest_execution_date(session=session, execution_date=execution_date)
-
-        # If no execution_date could be found, sleep the grace period, and try again once.This will sleep the duration
-        # of the grace period and try again. This is an alternative to scheduling the workflow at a slightly later time
-        # to allow Airflow to record the dagrun in the database.
-        # Note that this occupies a slot in execution queue.
-        if date is None:
-            sleep(self.grace_period.total_seconds())
-            date = self.get_latest_execution_date(session=session, execution_date=execution_date)
-
-        if not date:
-            return success_state
-
-        task_done = self.is_dagrun_done(date=date, session=session)
-
-        if task_done:
-            return success_state
-
-        return retry_state
-
-    def get_latest_execution_date(
-        self, *, session: scoped_session, execution_date: datetime.datetime
-    ) -> Union[datetime.datetime, None]:
-        """Get the most recent execution date for a task in a given DAG.
-
-        :param sesssion: Db session.
-        :param execution_date: Current execution date.
-        :return: Most recent execution date in the window.
-        """
-
-        end_date = execution_date
-        start_date = end_date - self.duration
-
-        response = (
-            session.query(DagRun)
-            .filter(
-                DagRun.dag_id == self.external_dag_id,
-                DagRun.execution_date.between(start_date, end_date),
-            )
-            .all()
+        super().__init__(
+            task_id=task_id,
+            external_dag_id=external_dag_id,
+            mode=mode,
+            poke_interval=poke_interval,
+            timeout=timeout,
+            check_existence=check_existence,
+            execution_date_fn=execution_date_fn,
+            **kwargs,
         )
 
-        dates = [r.execution_date for r in response]
-        dates.sort(reverse=True)
 
-        if len(dates) > 0:
-            return dates[0]
+@provide_session
+def latest_execution_timedelta(
+    data_interval_start: datetime, ext_dag_id: str, session: scoped_session = None, **context
+) -> int:
+    """
+    Get the latest execution for a given external dag and returns its data_interval_start (logical date)
 
-        return None
-
-    def check_dag_exists(self, session: scoped_session):
-        """Check if the DAG exists in the DB.  If they do not, then throw an exception.
-
-        :param session: Database session.
-        """
-
-        dag_model = session.query(DagModel).filter(DagModel.dag_id == self.external_dag_id).first()
-        if dag_model is None or not os.path.exists(dag_model.fileloc):
-            raise AirflowException(f"DAG: {self.external_dag_id} does not exist.")
-
-    def is_dagrun_done(self, *, date: datetime.datetime, session: scoped_session) -> bool:
-        """Check if the dagrun has finished executing at a given execution date.
-        :param date: execution date.
-        :return: True if finished, False otherwise.
-        """
-
-        success_states = [State.SUCCESS]
-        response = (
-            session.query(DagRun)
-            .filter(
-                DagRun.dag_id == self.external_dag_id,
-                DagRun.execution_date.in_([date]),
-                DagRun.state.in_(success_states),
-            )
-            .first()
+    :param ext_dag_id: The dag_id to get the latest execution date for.
+    :return: The latest execution date in the window.
+    """
+    dagruns = (
+        session.query(DagRun)
+        .filter(
+            DagRun.dag_id == ext_dag_id,
         )
+        .all()
+    )
+    dates = [d.data_interval_start for d in dagruns]  # data_interval start is what ExternalTaskSensor checks
+    dates.sort(reverse=True)
 
-        if response is not None:
-            return True
+    if not len(dates):  # If no execution is found return the logical date for the Workflow
+        logging.warn(f"No Executions found for dag id: {ext_dag_id}")
+        return data_interval_start
 
-        return False
+    return dates[0]
